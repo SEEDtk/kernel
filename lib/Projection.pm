910de8131bc6a4f29a8d0231780debc4010f86db
Ross Overbeek <RossOverbeek@gmail.com>
2015-02-27 09:38:29 -0600

    with length statistics

--- lib/Projection.pm
+++ lib/Projection.pm
@@ -32,7 +32,8 @@ sub relevant_projection_data {
     {
 	my ( $vc, $peg, $function, $seq, $contig, $begin, $strand ) = @$tuple;
 	$seqs{$peg} = $seq;
-	$by_vc{$vc}->{$peg}++;
+	my $genome = &SeedUtils::genome_of($peg);
+	$by_vc{$vc}->{$genome}->{$peg}++;
 	$to_func{$peg} = $function;
 	$func_to_pegs{$function}->{$peg} = 1;
 	$peg2loc{$peg} = [$contig,$begin,$strand];
@@ -42,13 +43,64 @@ sub relevant_projection_data {
     $state->{to_func} = \%to_func;
     $state->{func_to_pegs} = \%func_to_pegs;
     $state->{peg2loc} = \%peg2loc;
-
+    $state->{length_stats} = &length_stats_by_family($state);
     return $state;
+}
+sub length_stats_by_family {
+    my($state) = @_;
+
+    my $len_stats = {};
+    my $func_to_pegs = $state->{func_to_pegs};
+    my @funcs        = keys(%$func_to_pegs);
+    foreach my $func (@funcs)
+    {
+	my @lengths;
+	my $pegH = $func_to_pegs->{$func};
+	if ($pegH)
+	{
+	    my @pegs = keys(%$pegH);
+	    if (@pegs >= 5)          # require 5 pegs for stats
+	    {
+		foreach my $peg (@pegs)
+		{
+		    my $tran = $seqs->{$peg};
+		    if ($tran)
+		    {
+			push(@lengths,length($tran));
+		    }
+		    else
+		    {
+			print STDERR "no translation for $peg\n";
+		    }
+		}
+		my($mean,$stddev) = &gjostat::mean_stddev(@lengths);
+		$len_stats->{$func} = [$mean,$stddev];
+	    }
+	    else
+	    {
+		print STDERR "too few pegs for function $func\n";
+	    }
+	}
+	else
+	{
+	    print STDERR "no pegs for $func\n";
+	}
+    }
+    return $len_stats;
 }
 
 sub create_recognition_parameters {
     my($state,$dataD,$shrub) = @_;
 
+    if (! -d $dataD)
+    {
+	mkdir($dataD,0777) || die "could not make $dataD";
+    }
+    open(PARMS,">$dataD/parameters") || die "could not open $parms";
+    my $json = JSON::XS->new;
+    $json->pretty(1);
+    print PARMS $json->encode($state);
+    close(PARMS);
 }
 
 sub project_solid_rows {
