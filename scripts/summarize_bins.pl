a98ce196c3663cb68fb22251022e1903c2ec93be
Ross Overbeek <RossOverbeek@gmail.com>
2015-04-29 14:51:57 -0500

    show multiple contigs for one uni role

--- scripts/summarize_bins.pl
+++ scripts/summarize_bins.pl
@@ -5,12 +5,22 @@ use ScriptUtils;
 my $opt =
   ScriptUtils::Opts( '',
                      ScriptUtils::ih_options(),
+		     [ 'c2uni|c=s','File connecting contigs to uni roles',{ required => 1}],
                      [ 'c1|u=f', 'incr for universal match', { default => 1 } ],
                      [ 'c2|e=f', 'decr for duplicate universal match', { default => 5} ]
     );
 my $ih = ScriptUtils::IH( $opt->input );
+my $c2uni = $opt->c2uni;
 my $c1 = $opt->c1;
 my $c2 = $opt->c2;
+
+my %uni2contigs;
+foreach $_ (`cat $c2uni`)
+{
+    chomp;
+    my($contig,$role) = split(/\t/,$_);
+    push(@{$uni2contigs{$role}},$contig);
+}
 
 $/ = "\n//\n";
 
@@ -18,22 +28,23 @@ while (defined ($_ = <STDIN>))
 {
     $/ = "\n";
     # Process the data for a bin.
-    &process_1($_);
+    &process_1($_,\%uni2contigs);
     $/ = "\n//\n";
 }
 
 sub process_1 {
-    my($x) = @_;
+    my($x,$uni2contigs) = @_;
     # Split the universal roles and the contig list.
     if ($x =~ /^(.*)\n\n(.*)\n?\/\/\n/s)
     {
         my $contigs = $1;
         my $univs = $2;
-	my @univ = map { ($_ =~ /^(\d+)/) ? $1 : () } split(/\n/,$univs);
+	my @univ = map { ($_ =~ /^(\d+)\t(\S.*\S)/) ? [$1,$2] : () } split(/\n/,$univs);
 	my $num_univ = @univ;
-	my @extra = grep { $_ > 1 } @univ;
+	my @extra = grep { $_->[0] > 1 } @univ;
 	my $num_extra = @extra;
-        print $univs,"\n--------\n";
+	&display_univ(\@univ,$uni2contigs);
+        print "\n--------\n";
         my @counts = &count_ref($contigs);
         foreach my $tuple (@counts)
         {
@@ -50,7 +61,24 @@ sub process_1 {
     }
 }
 
+sub display_univ {
+    my($univs,$uni2contigs) = @_;
 
+    foreach my $tuple (@$univs)
+    {
+	my($n,$role) = @$tuple;
+	print join("\t",@$tuple),"\n";
+	my $contigs = $uni2contigs->{$role};
+	if ($contigs && ($n > 1))
+	{
+	    foreach my $contig (@$contigs)
+	    {
+		print "\t$contig\n";
+	    }
+	}
+    }
+}
+ 
 sub count_ref {
     my($contigs) = @_;
 
