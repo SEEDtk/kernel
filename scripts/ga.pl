8704502a0226c755de315a077f138be1b600b652
Bruce Parrello <bparrello@figresearch.com>
2015-08-19 15:19:07 -0500

    Rather not say in public.

--- scripts/ga.pl
+++ scripts/ga.pl
@@ -41,7 +41,7 @@ sub score {
     my($chromosome,$iter,$dir,$log,$n) = @_;
     my $args = join(" ",$dir,"$log/$iter.$n",@$chromosome);
 
-    open(SC,"bins_compute $args |") || die "bins_compute $args: $!";
+    open(SC,"bins_score $args |") || die "bins_score $args: $!";
     my $sc = <SC>;
     chomp $sc;
     close(SC);
