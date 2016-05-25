e2541b40aeb0ec28bc538e1a573fe8607c68ebb9
Bruce Parrello <bparrello@figresearch.com>
2016-05-25 07:22:40 -0500

    HMP sample downloading script.

---
+++ scripts/SampleDownload.pl
@@ -0,0 +1,94 @@
+#!/usr/bin/env perl
+#
+# Copyright (c) 2003-2015 University of Chicago and Fellowship
+# for Interpretations of Genomes. All Rights Reserved.
+#
+# This file is part of the SEED Toolkit.
+#
+# The SEED Toolkit is free software. You can redistribute
+# it and/or modify it under the terms of the SEED Toolkit
+# Public License.
+#
+# You should have received a copy of the SEED Toolkit Public License
+# along with this program; if not write to the University of Chicago
+# at info@ci.uchicago.edu or the Fellowship for Interpretation of
+# Genomes at veronika@thefig.info or download a copy from
+# http://www.theseed.org/LICENSE.TXT.
+#
+
+
+use strict;
+use warnings;
+use FIG_Config;
+use ScriptUtils;
+use File::Copy::Recursive;
+
+=head1 Download Human Microbiome Samples
+
+    SampleDownload.pl [ options ] workDir
+
+This script downloads Human Microbiome Project samples from the NCBI web site. It accepts as input
+a tab-delimited file consisting of a site ID in the first column (e.g. C<palatine_tonsils>) and a
+sample ID in the second column (e.g. C<SRS015061>) for each sample to be downloaded. The samples
+are downloaded and unpacked. They must then be fed through the pipeline individually. (This will
+presumably be done in parallel.)
+
+=head2 Parameters
+
+The command-line options are those found in L<ScriptUtils/ih_options> (for the standard input). The
+positional parameter is the name of a work directory. Each sample is loaded into a subdirectory of this 
+work directory.
+
+=cut
+
+# Get the command-line parameters.
+my $opt = ScriptUtils::Opts('workDir', ScriptUtils::ih_options(),
+        );
+# Verify the work directory.
+my ($workDir) = @ARGV;
+if (! $workDir) {
+    die "No working directory specified.";
+} elsif (! -d $workDir) {
+    die "Invalid working directory $workDir specified.";
+}
+# Count the samples.
+my $count = 0;
+# Open the input file.
+my $ih = ScriptUtils::IH($opt->input);
+# Loop through the input.
+while (! eof $ih) {
+    # Switch to the working directory.
+    chdir $workDir;
+    # Get the next sample.
+    my $line = <$ih>;
+    chomp $line;
+    my ($site, $sample) = split /\t/, $line;
+    $count++;
+    print "Processing $count: $sample from $site.\n";
+    my $sampleDir = "$workDir/$sample";
+    if (! -d $sampleDir) {
+        File::Copy::Recursive::pathmk($sampleDir);
+    }
+    # Now we have a directory into which we can put the files. Download and unpack the sample reads.
+    my $rc = system('curl', '-O', "http://downloads.hmpdacc.org/data/Illumina/$site/$sample.tar.bz2");
+    die "Error code $rc downloading sample." if $rc;
+    $rc = system('tar', '-xzvf', "$sample.tar.bz2");
+    die "Error code $rc unpacking sample." if $rc;
+    # Delete the tar file.
+    unlink "$sample.tar.bz2";
+    # Create the site file.
+    open(my $oh, '>', "$sample/site.tbl") || die "Could not open site file: $!";
+    my $siteName = join(' ', map { ucfirst $_ } split /_/, $site);
+    print $oh "HMP\t$site\t$siteName\n";
+    close $oh;
+    # Download the abundance profile.
+    chdir $sampleDir;
+    my $abundanceF = $sample . '_abundance_table.tsv.bz2';
+    $rc = system('curl', '-O', "http://downloads.hmpdacc.org/data/HMSCP/$sample/$abundanceF");
+    die "Error code $rc downloading abundance." if $rc;
+    $rc = system('tar', '-xzvf', $abundanceF);
+    die "Error code $rc downloading abundance." if $rc;
+    # Delete the tar file.
+    unlink $abundanceF;
+}
+print "All done.\n";
