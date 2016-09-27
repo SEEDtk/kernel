42f000065f0cb629671597610fd12eb3ba271441
Bruce Parrello <bparrello@figresearch.com>
2016-09-27 05:43:59 -0500

    Test of SRX024977 theory.

--- scripts/TestMaple.pl
+++ scripts/TestMaple.pl
@@ -1,17 +1,30 @@
 use strict;
 use FIG_Config;
 use ScriptUtils;
-use File::Copy;
+use Stats;
 
-my $opt = ScriptUtils::Opts('sampleDir');
-my ($sampleDir) = @ARGV;
-my $inputDir = "$FIG_Config::data/Inputs/Samples";
-my @subs = qw(HMP MH);
-for my $sub (@subs) {
-    my $subDir = "$inputDir/$sub";
-    opendir(my $wh, $subDir) || die "Could not open $sub directory: $!";
-    my @dirs = grep { substr($_,0,1) ne '.' && -d "$subDir/$_" } readdir $wh;
-    for my $dir (@dirs) {
-        copy("$subDir/$dir/site.tbl", "$sampleDir/$dir/site.tbl") || die "Copy failed for $dir: $!";
+my $opt = ScriptUtils::Opts('gfile');
+my ($qfile) = @ARGV;
+open(my $ih, "<$qfile") || die "Could not open $qfile: $!";
+my $mode = "";
+my $stats = Stats->new();
+while (! eof $ih) {
+    my $line = <$ih>;
+    $stats->Add(lines => 1);
+    if ($mode eq '@') {
+        $stats->Add(reads => 1);
+        chomp $line;
+        my $n = length($line);
+        for (my $i = 0; $i < $n; $i++) {
+            my $c = substr($line,$i,1);
+            $stats->Add("char-$c" => 1);
+        }
+    } else {
+        if (substr($line,0,1) eq '@') {
+            $mode = '@';
+        } else {
+            $mode = "";
+        }
     }
 }
+print "Counts:\n" . $stats->Show();
