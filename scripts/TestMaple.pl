cd3c7f2f0fcf3ca796a62ce42b45b2bf5c9001c9
Bruce Parrello <bparrello@figresearch.com>
2016-11-25 13:50:54 -0600

    Emergency test script.

--- scripts/TestMaple.pl
+++ scripts/TestMaple.pl
@@ -4,39 +4,16 @@ use ScriptUtils;
 use Stats;
 
 $| = 1;
-my $opt = ScriptUtils::Opts('gfile');
-my ($qfile) = @ARGV;
-open(my $ih, "<$qfile") || die "Could not open $qfile: $!";
-my $mode = "";
-my $stats = Stats->new();
-while (! eof $ih) {
-    my $line = <$ih>;
-    my $count = $stats->Add(lines => 1);
-    print "$count lines read.\n" if ($count % 10000 == 0);
-    if ($mode eq '@') {
-        $stats->Add(reads => 1);
-        chomp $line;
-        my $n = length($line);
-        my $ns = ($line =~ tr/ACGT//);
-        my $xs = ($line =~ tr/N//);
-        if ($ns == $n) {
-            $stats->Add(goodLine => 1);
-        } elsif ($ns == 0) {
-            $stats->Add(badLine => 1);
-        }
-        if ($xs == $n) {
-            $stats->Add(nLine => 1);
-        }
-        $stats->Add(nChar => $xs);
-        $stats->Add(goodChar => $ns);
-        $stats->Add(badChar => ($n - $ns));
-        $mode = "";
-    } else {
-        if (substr($line,0,1) eq '@') {
-            $mode = '@';
-        } else {
-            $mode = "";
+my $opt = ScriptUtils::Opts('dir');
+my ($dir) = @ARGV;
+opendir(my $dh, $dir) || die "Could not open directory $dir: $!";
+my @packages = grep { $_ =~ /^\d+\.\d+$/ } readdir $dh;
+for my $package (@packages) {
+    open(my $ih, '<', "$dir/$package/data.tbl") || die "Could not open data file for $package: $!";
+    while (! eof $ih) {
+        my $line = <$ih>;
+        if ($line =~ /\t$/) {
+            print "Anomaly in $package.\n";
         }
     }
 }
-print "Counts:\n" . $stats->Show();
