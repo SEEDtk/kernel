1eb41a0b8db4d773ce5237f2ff889f9897a50739
Bruce Parrello <bparrello@figresearch.com>
2017-05-18 11:27:17 -0500

    More new binning.

--- scripts/PheS_evidence_of_reps.pl
+++ scripts/PheS_evidence_of_reps.pl
@@ -2,7 +2,9 @@ use SeedUtils;
 use strict;
 use Data::Dumper;
 use RepKmers;
-use gjoseqlib;
+use FastA;
+use FastQ;
+use ScriptUtils;
 
 =head1 Use PheS Signatures to Predict Presence of Representative Genomes
 
@@ -11,10 +13,10 @@ use gjoseqlib;
 The Data.PheS directory must contain
 
     complete.genomes a 2-column table [GenomeId,GenomeName]
-                     that defines the set of genomes for 
+                     that defines the set of genomes for
                      which PheS signatures are computed
 
-    6.1.1.20.fasta   which is a collection of PheS aa sequences.  These need 
+    6.1.1.20.fasta   which is a collection of PheS aa sequences.  These need
                      to include all genomes in complete.genomes
 
     PheS.signatures  a 2-column table  [Signature,GenomeId]
@@ -25,83 +27,101 @@ The Data.PheS directory must contain
 
 =head2 Input
 
-The input is a fasta sequence of DNA fragments (usually contigs or reads)
+The input is a file of DNA fragments (usually contigs (FASTA) or reads (FASTQ)).
 
-=head2 Ouput
+=head2 Output
 
 Executing the command produces a report suggesting which
 of the representative genomes are present in the input file
 
 =cut
 
+my $opt = ScriptUtils::Opts('DataDir file1 file2',
+        ['fasta', 'input is in FASTA format'],
+        ['kmerSize|kmer|k=i', 'kmer size (default 8)', { default => 8 }]);
 (my $dataD = shift @ARGV)
     || die "usgae: pheS_evidence_of_reps Data";
-
-my $K=8;
-if (-s "$dataD/K") {
-    $K= &SeedUtils::file_head("$dataD/K", 1);
-    chomp $K;
-}
+my ($file1, $file2) = @ARGV;
+my $count = 0;
+my $hitCount = 0;
+my $K= $opt->kmersize;
 
 my($g2s,$s2g,$g2name) = &load_PheS_sigs($dataD);
 
-my $hits = {};
-while (my $tuple = &gjoseqlib::read_next_fasta_seq(\*STDIN))
-{
-    my $seq = $tuple->[2];
-    &process_seq(\$seq,$g2s,$s2g,$K,$hits)
+my $reader;
+if ($opt->fasta) {
+    $file1 //= \*STDIN;
+    $reader = FastA->new($file1);
+} else {
+    $file1 //= \*STDIN;
+    $reader = FastQ->new($file1, $file2);
 }
+my $hits = {};
+while ($reader->next())
+{
+    my $seq = $reader->left;
+    &process_seq(\$seq,$g2s,$s2g,$K,$hits);
+    my $seq2 = $reader->right;
+    &process_seq(\$seq,$g2s,$s2g,$K,$hits)
 
+}
+print STDERR "Analyzing the hits.\n";
 &process_hits($g2s, $s2g, $g2name, $hits);
 
 sub process_hits {
     my($g2s, $s2g, $g2name, $hits) = @_;
-    
+
     my %num_sigs_for_g;
     foreach my $g (keys % { $g2s })
     {
-	my $sigs = $g2s->{$g};
-	foreach my $sig (keys %$sigs)
-	{
-	    ++$num_sigs_for_g{$g};
-	}
+        print STDERR "Counting hits for $g.\n";
+        my $sigs = $g2s->{$g};
+        foreach my $sig (keys %$sigs)
+        {
+            ++$num_sigs_for_g{$g};
+        }
     }
-    
+
 #     print STDOUT map { (join("\t", @$_), "\n")
 #     } sort { $b->[1] <=> $a->[1]
 #     } map { [$_, (scalar keys % { $hits->{$_} })] } keys % { $hits };
 #     die "aborted";
-    
+
     my %num_hits_for_g;
     foreach my $g (keys % { $hits })
     {
-	my $sigs = $hits->{$g};
-	foreach my $sig (keys %$sigs)
-	{
-	    ++$num_hits_for_g{$g};
-	}
+        print STDERR "Summarizing hits for $g.\n";
+        my $sigs = $hits->{$g};
+        foreach my $sig (keys %$sigs)
+        {
+            ++$num_hits_for_g{$g};
+        }
     }
-    
+
     my %frac_hits;
     foreach my $g (keys % { $g2s })
     {
-	$frac_hits{$g} = $num_hits_for_g{$g} / $num_sigs_for_g{$g}
+        $frac_hits{$g} = $num_hits_for_g{$g} / $num_sigs_for_g{$g}
     }
-    
+
     foreach my $g (sort { $frac_hits{$b} <=> $frac_hits{$a} } keys %frac_hits)
     {
-	next if ($frac_hits{$g} == 0.0);
-	print STDOUT (join("\t", ($g, 100.0*$frac_hits{$g}, $g2name->{$g})), "\n");
+        next if ($frac_hits{$g} == 0.0);
+        print STDOUT (join("\t", ($g, 100.0*$frac_hits{$g}, $g2name->{$g})), "\n");
     }
 }
 
 
 sub process_seq {
     my($seqP,$g2s,$s2g,$K,$hits) = @_;
-    
+
     &process_strand($seqP,$g2s,$s2g,$K,$hits);
     &rev_comp($seqP);    #...rev_comp value is returned "In-place"...
     &process_strand($seqP,$g2s,$s2g,$K,$hits);
+    $count++;
+    if ($count % 100000 == 0) {
+        print STDERR "$count sequences processed. $hitCount hits.\n";
+    }
 }
 
 sub process_strand {
@@ -110,19 +130,20 @@ sub process_strand {
     my $offset;
     for ($offset=0; ($offset <= 2); $offset++)
     {
-	my $tmp = substr($$seqP,$offset);
-	my $aa_seq = &SeedUtils::translate($tmp,undef,0);
-	
-	my $i;
-	my $last = (length($aa_seq)- 1) - $K;
-	for ($i=0; ($i <= $last); $i++)
-	{
-	    my $kmer = uc substr($aa_seq,$i,$K);
-	    if (my $g = $s2g->{$kmer})
-	    {
-		$hits->{$g}->{$kmer} = 1;
-	    }
-	}
+        my $tmp = substr($$seqP,$offset);
+        my $aa_seq = &SeedUtils::translate($tmp,undef,0);
+
+        my $i;
+        my $last = (length($aa_seq)- 1) - $K;
+        for ($i=0; ($i <= $last); $i++)
+        {
+            my $kmer = uc substr($aa_seq,$i,$K);
+            if (my $g = $s2g->{$kmer})
+            {
+                $hits->{$g}->{$kmer} = 1;
+                $hitCount++;
+            }
+        }
     }
 }
 
@@ -132,20 +153,20 @@ sub load_PheS_sigs {
     my $g2s = {};
     my $s2g = {};
     open(SIGS,"<$dataD/PheS.signatures")
-	|| die "could not open $dataD/PheS.signatures";
+        || die "could not open $dataD/PheS.signatures";
     while (defined($_ = <SIGS>))
     {
-	if ($_ =~ /^(\S+)\t(\S+)/)
-	{
-	    my($sig,$gid) = ($1,$2);
-	    $g2s->{$gid}->{$sig}++;
-	    $s2g->{$sig} = $gid;
-	}
+        if ($_ =~ /^(\S+)\t(\S+)/)
+        {
+            my($sig,$gid) = ($1,$2);
+            $g2s->{$gid}->{$sig}++;
+            $s2g->{$sig} = $gid;
+        }
     }
     close(SIGS);
-    
+
     my $g2name;
     %$g2name = map { m/^(\S+)\t(\S.*\S)/ ? ($1 => $2) : () } &SeedUtils::file_read("$dataD/complete.genomes");
-    
+
     return ($g2s,$s2g,$g2name);
 }
