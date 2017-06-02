8e58863f15ba504a61364023cb1ffe5d4be83e8e
Bruce Parrello <bparrello@figresearch.com>
2017-06-02 16:11:26 -0500

    new projection document

---
+++ scripts/ProjectionRepo.pl
@@ -0,0 +1,122 @@
+#!/usr/bin/env perl
+#
+# Copyright (c) 2003-2015 University of Chicago and Fellowship
+# for Interpretations of Genomes. All Rights Reserved.
+#
+# This file is part of the SEED Toolkit.
+#
+# The SEED Toolkit is free software. You can redistribute
+# it and/or modify it under the terms of the SEED Toolkit
+# Public License.
+#
+# You should have received a copy of the SEED Toolkit Public License
+# along with this program; if not write to the University of Chicago
+# at info@ci.uchicago.edu or the Fellowship for Interpretation of
+# Genomes at veronika@thefig.info or download a copy from
+# http://www.theseed.org/LICENSE.TXT.
+#
+
+
+use strict;
+use warnings;
+use FIG_Config;
+use Shrub;
+use ScriptUtils;
+use File::Copy::Recursive;
+use Stats;
+
+=head1 Create Projection Repository
+
+    ProjectionRepo.pl [ options ] outDir
+
+This script creates the Projection Repository described in L<ProjectionChallenge>. The repository will be restricted to the current
+set of well-behaved genomes in the Shrub. As currently constructed, it will only contain PEG features.
+
+=head2 Parameters
+
+The positional parameter is the name of the output directory. A C<GenomeData> and a C<SubsystemsData> directory will be created
+underneath this named output directory.
+
+The command-line options are those found in L<Shrub/script_options> plus the following.
+
+=over 4
+
+=item clear
+
+If the output directory already exists, erase it before starting. The default is to fail if the output directory already exists.
+
+=cut
+
+$| = 1;
+# Get the command-line parameters.
+my $opt = ScriptUtils::Opts('outDir',
+        Shrub::script_options(),
+        ['clear|C', 'erase output directory before creating the repository']
+        );
+# Create the statistics object.
+my $stats = Stats->new();
+# Connect to the database.
+my $shrub = Shrub->new_for_script($opt);
+# Find the output directory.
+my ($outDir) = @ARGV;
+if (! $outDir) {
+    die "No output directory specified.";
+} elsif (-d $outDir) {
+    if ($opt->clear) {
+        print "Erasing $outDir.\n";
+        File::Copy::Recursive::pathempty($outDir);
+    } else {
+        die "Directory $outDir already exists.";
+    }
+} else {
+    print "Creating $outDir.\n";
+    create_dir($outDir);
+}
+# Locate the DNA repository.
+my $dnaRepo = $shrub->DNArepo();
+# Create the GenomeData directory.
+print "Creating GenomeData.\n";
+my $genomeRoot = "$outDir/GenomeData";
+create_dir($genomeRoot);
+# Get all the well-behaved genomes.
+print "Loading genome list.\n";
+my %genomes = map { $_->[0] => [$_->[1], $_->[2]] } $shrub->GetAll('Genome', 'Genome(well-behaved) = ?', [1], 'id name contig-file');
+my @gList = sort keys %genomes;
+my $gCount = scalar @gList;
+print "$gCount genomes found.\n";
+# Loop through the genomes.
+my $gNum = 0;
+for my $genome (@gList) {
+    ++$gNum;
+    my ($gName, $contigFile) = @{$genomes{$genome}};
+    print "Processing $genome $gName ($gNum of $gCount).\n";
+    # Create the genome's directory.
+    my $genomeDir = "$genomeRoot/$genome";
+    create_dir($genomeDir);
+    # Output the type file.
+    create_list("$genomeDir/name", [$gName]);
+    # Copy the contig file.
+    File::Copy::Recursive::fcopy("$dnaRepo/$contigFile", "$genomeDir/contigs");
+    $stats->Add(contigFilesCopied => 1);
+    # Now we output the pegs.
+
+}
+
+# Create a simple list file.
+sub create_list {
+    my ($fileName, $itemList) = @_;
+    open(my $oh, ">$fileName") || die "Could not open $fileName: $!";
+    $stats->Add(listFileCreated => 1);
+    for my $item ($itemList) {
+        print $oh "$item\n";
+        $stats->Add(listItemOut => 1);
+    }
+    close $oh;
+}
+
+# Create a directory.
+sub create_dir {
+    my ($dirName) = @_;
+    File::Copy::Recursive::pathmk($dirName) || die "Could not create directory $dirName: $!";
+    $stats->Add(dirCreated => 1);
+}
