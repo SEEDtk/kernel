52b36f481a6ad650f703837157d38470b859256e
Bruce Parrello <bparrello@figresearch.com>
2017-11-10 12:20:17 -0600

    New coupling with experimental subsystems included.

---
+++ scripts/core_role_table.pl
@@ -0,0 +1,145 @@
+#!/usr/bin/env perl
+#
+# Copyright (c) 2003-2015 University of Chicago and Fellowship
+# for Interpretations of Genomes. All Rights Reserved.
+#
+# This file is part of the SEED Toolkit.
+#
+# The SEED Toolkit is free software. You can redistribute
+# it and/or modify it under the terms of the SEED Toolkit
+# Public License.
+#
+# You should have received a copy of the SEED Toolkit Public License
+# along with this program; if not write to the University of Chicago
+# at info@ci.uchicago.edu or the Fellowship for Interpretation of
+# Genomes at veronika@thefig.info or download a copy from
+# http://www.theseed.org/LICENSE.TXT.
+#
+
+
+use strict;
+use warnings;
+use FIG_Config;
+use Shrub;
+use ScriptUtils;
+use CopyFromSeed;
+use Stats;
+use RoleParse;
+
+
+=head1 Build a Role Table From a FIGdisk
+
+    core_role_table.pl [ options ] coreDir outFile
+
+This script builds a C<roles.in.subsystems>-like file from a FIGdisk's Subsystems directory. All subsystems are included.
+(See L<build_role_tables.pl> for a description of the C<roles.in.subsystems> file.) This file can be used to identify a
+larger set of subsystem-annotated roles since it will include experimental subsystems. Note, however, that
+only roles currently in the L<Shrub> database will be processed, and only exchangeable subsystems will be processed.
+
+The output file will be tab-delimited, with four columns: (0) role ID, (1) role hash code, (2) role name, and (3) "X" if
+the role is from an experimental subsystem.
+
+=head2 Parameters
+
+The positional parameters are the name of the FIGdisk directory and the name to give to the output file.
+
+The command-line options are those found in L<Shrub/script_options>.
+
+=cut
+
+# Get the command-line parameters.
+my $opt = ScriptUtils::Opts('coreDir outFile',
+            Shrub::script_options(),
+        );
+# Create the statistics object.
+my $stats = Stats->new();
+# Connect to the database.
+my $shrub = Shrub->new_for_script($opt);
+# Get the parameters.
+my ($coreDir, $outFile) = @ARGV;
+if (! $coreDir) {
+    die "No SEED directory specified.";
+} elsif (! -d "$coreDir/FIG/Data/Subsystems") {
+    die "$coreDir does not appear to be a SEED FIGdisk.";
+} elsif (! $outFile) {
+    $outFile = "roles.in.all.subsytems";
+    print "Default output file name used.\n";
+}
+# Get the subsystem list.
+my $subsysDir = "$coreDir/FIG/Data/Subsystems";
+opendir(my $dh, $subsysDir) || die "Could not open subsystem directory: $!";
+my @subs = grep { -f "$subsysDir/$_/spreadsheet" } readdir $dh;
+closedir $dh;
+my $total = scalar @subs;
+print "$total subsystem directories found.\n";
+# This hash will map each role ID to its checksum and name.
+my %roles;
+# This hash maps each checksum to a role ID.
+my %checksums;
+# This will count the subsystems.
+my $counter = 0;
+# This hash will count the number of non-experimental subsystems containing each role. When we
+# unspool the output, roles not in here will get the X at the end.
+my %nonExp;
+for my $sub (sort @subs) {
+    $counter++;
+    print "Processing $sub ($counter of $total).\n";
+    my $subDir = "$subsysDir/$sub";
+    my $public = CopyFromSeed::ReadFlagFile("$subDir/EXCHANGABLE");
+    if (! $public) {
+        print "Private subsystem skipped.\n";
+        $stats->Add(subsystemPrivate => 1);
+    } else {
+        # This will be set to TRUE if we are experimental.
+        my $exp;
+        if (open(my $ch, "$subDir/CLASSIFICATION")) {
+            my $line = <$ch>;
+            if ($line =~ /experimental/i) {
+                $exp = 1;
+                print "Subsystem $sub is experimental.\n";
+                $stats->Add(subsystemExperimental => 1);
+            }
+        }
+        # Now we are ready to read the roles.
+        open(my $sh, "$subDir/spreadsheet") || die "Could not open $sub spreadsheet: $!";
+        my $line = '';
+        while (! eof $sh && $line ne '//') {
+            $line = <$sh>;
+            chomp $line;
+            if ($line =~ /^\S+\t(.+)/) {
+                # Here we have a role.
+                my $name = $1;
+                my $checksum = RoleParse::Checksum($name);
+                my $roleID;
+                $stats->Add(roleRead => 1);
+                if (exists $checksums{$checksum}) {
+                    $roleID = $checksums{$checksum};
+                    $stats->Add(roleOldFound => 1);
+                } else {
+                    $stats->Add(roleNewFound => 1);
+                    ($roleID) = $shrub->GetFlat('Role', 'Role(checksum) = ?', [$checksum], 'Role(id)');
+                    $checksums{$checksum} = $roleID;
+                    if (! $roleID) {
+                        $stats->Add(roleNotInShrub => 1);
+                    } else {
+                        $stats->Add(roleAdded => 1);
+                        $roles{$roleID} = [$checksum, $name];
+                    }
+                }
+                # If the role is not experimental, record that fact.
+                if ($roleID && ! $exp) {
+                    $nonExp{$roleID}++;
+                    $stats->Add(roleNotExperimental => 1);
+                }
+            }
+        }
+        close $sh;
+    }
+}
+# Now we have processed all the subsystems. Write out the roles.
+open(my $oh, ">$outFile") || die "Could not open output file: $!";
+for my $roleID (sort keys %roles) {
+    my $roleData = $roles{$roleID};
+    my $exp = ($nonExp{$roleID} ? '' : 'X');
+    print $oh join("\t", $roleID, @$roleData, $exp) . "\n";
+}
--- scripts/non_ss_coupled_to_subsystem.pl
+++ scripts/non_ss_coupled_to_subsystem.pl
@@ -1,12 +1,87 @@
+#!/usr/bin/env perl
+#
+# Copyright (c) 2003-2015 University of Chicago and Fellowship
+# for Interpretations of Genomes. All Rights Reserved.
+#
+# This file is part of the SEED Toolkit.
+#
+# The SEED Toolkit is free software. You can redistribute
+# it and/or modify it under the terms of the SEED Toolkit
+# Public License.
+#
+# You should have received a copy of the SEED Toolkit Public License
+# along with this program; if not write to the University of Chicago
+# at info@ci.uchicago.edu or the Fellowship for Interpretation of
+# Genomes at veronika@thefig.info or download a copy from
+# http://www.theseed.org/LICENSE.TXT.
+#
+
 use strict;
 use Data::Dumper;
 use Shrub;
 use ScriptUtils;
 
+=head1 Create Coupling Report
+
+    non_ss_coupled_to_subsystem.pl [ options ]
+
+This script creates the final coupling report. It searches for roles not in subsystems that are coupled to subsystem roles.
+The output file is tab-delimited, and divided into sections. Each section contains a header line specifying the non-subsystem
+role and one or more data lines specifying the clustered roles.
+
+The header line fields are (0) the maximum coupling count for
+the role, (1) the role description, and (2) the ID of a feature that exemplifies the role. This exemplar is chosen from features
+that are nearby as many coupled roles as possible.
+
+The data line fields are (0) a blank, indicating that this is a data line, (1) the name of the coupled role, and (2) an C<X> if the role
+is in an experimental subsystem.
+
+A line containing a separator (C<//>) divides sections with exemplar features from those without.
+
+=head2 Parameters
+
+There are no positional parameters.
+
+The command-line options are those found in L<Shrub/script_options> plus the following.
+
+=over 4
+
+=item gap
+
+Region size in each direction for finding clustered features. The default is C<5000>.
+
+=item roles
+
+The input file specifying which roles are in subsystems. The default is C<roles.in.all.subsystems>. This file is tab-delimited, and
+each record contains (0) a role ID, (1) a role checksum, (2) the role name, and (3) an C<X> if the role is in an experimental subsystem.
+The default is C<roles.in.all.subsystems>.
+
+=item roleCouples
+
+A file containing the role couples. This file is tab-delimited with headers, and each record contains (0) the first role name, (1) the
+second role name, and (2) the number of coupling instances. The default is C<role.couples.tbl>.
+
+=item min
+
+The minimum number of coupling instances required for a pair to be significant. The default is C<100>.
+
+=head2 Procedure
+
+To generate the input files for this program, create a special directory and use the following commands.
+
+    all_entities Genome | get_feature_coupling_table –-headers >coupling.tbl 2>coupling.log
+    p3-find-couples --location=location --sequence=contig role <coupling.tbl >role0.couples.tbl
+    expand_coupled_roles <role0.couples.tbl >role.couples.tbl
+    core_role_table /vol/core-seed/FIGdisk roles.in.all.subsystems >roles.log
+    non_ss_coupled_to_subsystem >coupling.report.txt
+
+=cut
+
+
 $| = 1;
 my $opt = ScriptUtils::Opts('', Shrub::script_options(),
         ['gap=i', 'region size in each direction', { default => 5000 }],
-        ['roles=s', 'roles.in.subsystems file', { default => 'roles.in.subsystems'}],
+        ['roles=s', 'roles.in.subsystems file', { default => 'roles.in.all.subsystems'}],
         ['roleCouples=s', 'role couples file', { default => 'role.couples.tbl' }],
         ['min=i', 'minimum count', { default => 100 }]);
 my $roles_in_ssF  = $opt->roles;
@@ -16,7 +91,17 @@ my $shrub         = Shrub->new_for_script($opt);
 my $gap           = $opt->gap;
 
 open(INSS,"<$roles_in_ssF") || die "could not open $roles_in_ssF";
-my %in_subsys = map { ($_ =~ /^([^\t]+)\t[^\t]*\t(\S.*)$/) ? ($2 => $1) : () } <INSS>;
+my %exp;
+my %in_subsys;
+while (! eof INSS) {
+    my $line = <INSS>;
+    chomp $line;
+    my ($id, undef, $name, $exp) = split /\t/, $line;
+    $in_subsys{$name} = $id;
+    if ($exp) {
+        $exp{$name} = 1;
+    }
+}
 close(INSS);
 
 my %poss;
@@ -67,7 +152,8 @@ foreach my $tuple (@sorted)
     push @$reportL, "$count\t$r1\t$peg\n";
     my @couples = sort { $coupled->{$b} <=> $coupled->{$a} } keys %$coupled;
     for my $couple (@couples) {
-        push @$reportL, "\t$coupled->{$couple}\t$couple\n";
+        my $exp = ($exp{$couple} ? 'X' : '');
+        push @$reportL, "\t$coupled->{$couple}\t$couple\t$exp\n";
     }
     if ($peg) {
         print @$reportL;
