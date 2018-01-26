fb35772804b34d1393ece34d9dd298bdeebf22e2
Bruce Parrello <bparrello@figresearch.com>
2018-01-26 10:45:10 -0600

    Added progress messages.

--- scripts/PheS_generate_signatures.pl
+++ scripts/PheS_generate_signatures.pl
@@ -11,10 +11,10 @@ use gjoseqlib;
 The Data.PheS directory must contain
 
     complete.genomes a 2-column table [GenomeId,GenomeName]
-                     that defines the set of genomes for 
+                     that defines the set of genomes for
                      which PheS signatures are computed
 
-    6.1.1.20.fasta   which is a collection of PheS aa sequences.  These need 
+    6.1.1.20.fasta   which is a collection of PheS aa sequences.  These need
                      to include all genomes in complete.genomes
 
 =head2 Ouput
@@ -29,18 +29,19 @@ the representative genome).
 
 (my $dataD = shift @ARGV)
     || die "usage: pheS_generate_signatures Data";
-
+print "Reading FASTA file.\n";
 my @tuples = gjoseqlib::read_fasta("$dataD/6.1.1.20.fasta");
 (@tuples > 0) || die "Missing $dataD/6.1.1.20.fasta";
 my %seqs = map { ($_->[1] => $_->[2]) } @tuples;
-           
+print "Reading genome list.\n";
 open(GENOMES,"<$dataD/complete.genomes")
     || die "Missing $dataD/complete.genomes";
 my %g_names = map { ($_ =~ /^(\S+)\t(\S.*\S)$/) ? ($1 => $2) : () } <GENOMES>;
 close(GENOMES);
 
 my @gids    = sort { $a <=> $b } keys(%g_names);
-
+my $total = scalar @gids;
+print "$total genomes found.\n";
 my $K = 8;
 if (open(K,"<$dataD/K"))
 {
@@ -48,12 +49,13 @@ if (open(K,"<$dataD/K"))
     close(K);
     if ($_ =~ /^(\d+)/)
     {
-	$K = $1;
+        $K = $1;
     }
 }
-
+print "Kmer size is $K.\n";
 # my $aa_alphabet = "ACDEFGHIKLMNPQRSTVWY";
 my %kmers;
+my $count = 0;
 foreach my $gid (@gids)
 {
     my $aa = uc $seqs{$gid};
@@ -62,22 +64,31 @@ foreach my $gid (@gids)
     my $last = (length($aa) - 1) - $K;
     for ($i = 0; ($i <= $last); $i++)
     {
-	my $x = substr($aa,$i,$K);
-	if ($x =~ /^[ACDEFGHIKLMNPQRSTVWY]{$K}$/o)
-	{
-	    $kmers{$x}->{$gid} = 1;
-	}
+        my $x = substr($aa,$i,$K);
+        if ($x =~ /^[ACDEFGHIKLMNPQRSTVWY]{$K}$/o)
+        {
+            $kmers{$x}->{$gid} = 1;
+        }
     }
+    $count++;
+    print "$count of $total genomes processed.\n" if $count % 100 == 0;
 }
 
 open(KMERS,">$dataD/PheS.signatures")
     || die "could not open $dataD/PheS.signatures";
+my $kTotal = scalar keys %kmers;
+my $found = 0;
+$count = 0;
 foreach my $kmer (keys(%kmers))
 {
     my $gidH = $kmers{$kmer};
     my @gids = keys(%$gidH);
     if (@gids == 1)
     {
-	print KMERS join("\t",($kmer,$gids[0])),"\n";
+        print KMERS join("\t",($kmer,$gids[0])),"\n";
+        $found++;
     }
+    $count++;
+    print "$count of $kTotal kmers processed. $found signatures found.\n" if $count % 1000 == 0;
 }
+print "All done. $found signatures found in $total genomes.\n";
