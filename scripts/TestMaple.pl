7d587d2b1f01860183c9c83d8e81e9a5229cf158
Bruce Parrello <bparrello@figresearch.com>
2018-01-31 14:41:45 -0600

    Quick check of LWP version.

--- scripts/TestMaple.pl
+++ scripts/TestMaple.pl
@@ -4,25 +4,6 @@ use ScriptUtils;
 use Shrub;
 use Stats;
 use SeedUtils;
+use LWP 5.66;
 
-my ($dir) = @ARGV;
-opendir(my $dh, $dir);
-my @subs = grep { -f "$dir/$_/contigs.fasta" } readdir $dh;
-for my $sub (@subs) {
-    my $workDir = "$dir/$sub";
-    opendir(my $dh, $workDir) || die "Could not open work directory: $!";
-    my @files = grep { -f "$workDir/$_" } readdir $dh;
-    print "Deleting intermediate files in $workDir.\n";
-    my ($count, $total) = (0,0);
-    for my $file (@files) {
-        my $fullName = "$workDir/$file";
-        $total++;
-        unless ($fullName =~ /abundance/ || $fullName =~ /\.fastq$/ || $fullName =~ /\.fq/ || $file eq 'site.tbl' ||
-            $file eq 'contigs.fasta' || $file eq 'output.contigs2reads.txt') {
-            unlink $fullName;
-            $count++;
-        }
-    }
-    print "$count of $total files deleted.\n";
-    print "$sub fixed.\n";
-}
+print "Checked.\n";
