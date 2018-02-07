38c794d50485afb9fcd0f0978074b4c3a0d0337f
Bruce Parrello <bparrello@figresearch.com>
2018-02-07 16:01:50 -0600

    Quick summary of binning results.

--- scripts/TestMaple.pl
+++ scripts/TestMaple.pl
@@ -1,9 +1,46 @@
 use strict;
 use FIG_Config;
 use ScriptUtils;
-use Shrub;
 use Stats;
-use SeedUtils;
-use LWP 5.66;
+use FastA;
 
-print "Checked.\n";
+my %binCounts; # #bins -> [count,min,max,tot]
+my $binDir = "$FIG_Config::data/Bins_HMP";
+opendir(my $dh, $binDir) || die "Could not open bins directory: $!";
+my @samples = grep { -s "$binDir/$_/bins.report.txt" } readdir $dh;
+for my $sample (@samples) {
+    print STDERR "Processing $sample.\n";
+    open(my $ih, "<$binDir/$sample/bins.report.txt") || die "Could not open bin report: $!";
+    my $binCount = 0;
+    while (! eof $ih) {
+        my $line = <$ih>;
+        if ($line =~ /^BIN \d/) {
+            $binCount++;
+        }
+    }
+    close $ih; undef $ih;
+    my $bp = 0;
+    if (open($ih, "<$binDir/$sample/contigs.fasta")) {
+        my $reader = FastA->new($ih);
+        while ($reader->next) {
+            my $seq = $reader->left;
+            $bp += length $seq;
+        }
+    }
+    my $countData = $binCounts{$binCount};
+    if (! $countData) {
+        $binCounts{$binCount} = [1, $bp, $bp, $bp];
+    } else {
+        $countData->[0]++;
+        $countData->[1] = $bp if $bp < $countData->[1];
+        $countData->[2] = $bp if $bp > $countData->[2];
+        $countData->[3] += $bp;
+    }
+}
+print STDERR "Writing results.\n";
+print "#bins\tnum\tmin\tmax\tmean\n";
+for my $count (sort { $a <=> $b } keys %binCounts) {
+    my ($num, $min, $max, $tot) = @{$binCounts{$count}};
+    my $mean = sprintf("%f.2", $tot/$num);
+    print join("\t", $num, $min, $max, $mean) . "\n";
+}
