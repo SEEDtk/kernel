83ac001ff872eb5a70cdfe58fe770f7aed21dee6
Bruce Parrello <bparrello@figresearch.com>
2018-02-16 09:59:19 -0600

    New site analysis for Sveta.

--- scripts/bins_site_analysis.pl
+++ scripts/bins_site_analysis.pl
@@ -24,16 +24,17 @@ use ScriptUtils;
 use Stats;
 use GenomeTypeObject;
 
-=head1 Find the Binning Directories for a Given Site
+=head1 Analyze the Binning Directories by Site
 
-    bins_site_analysis.pl [ options ] site_name binDir
+    bins_site_analysis.pl [ options ] binDir
 
-This script will search a binning jobs directory for bins from a given site with one or more genome results.
-For each bin, the sample ID and the number of good and bad output genomes will be displayed.
+This script will process a binning jobs directory and produce statistics on the binning jobs organized by site. It will
+display site name, number of good bins, number of bad bins, total base pairs, binned base pairs, and unbinned base pairs for each
+binning job. The output will be sorted by site.
 
 =head2 Parameters
 
-The positional parameters are the name of the site of interest and the directory containing the binning jobs.
+The positional parameter is the name of the directory containing the binning jobs.
 
 The command-line options are the following.
 
@@ -49,14 +50,12 @@ Name of the good-genome directory.
 
 $| = 1;
 # Get the command-line parameters.
-my $opt = ScriptUtils::Opts('site_name binDir',
+my $opt = ScriptUtils::Opts('binDir',
         ['good=s', 'good-genomes directory', { default => "$FIG_Config::data/GoodPackages"}],
         );
 # Get the parameters.
-my ($siteName, $binDir) = @ARGV;
-if (! $siteName) {
-    die "No site name specified.";
-} elsif (! $binDir) {
+my ($binDir) = @ARGV;
+if (! $binDir) {
     die "No bin job directory specified.";
 } elsif (! -d $binDir) {
     die "Bin job directory $binDir missing or invalid.";
@@ -73,59 +72,100 @@ print STDERR "Gathering bin jobs.\n";
 opendir($dh, $binDir) || die "Could not open bin job directory $binDir: $!";
 my @binJobs = sort grep { -f "$binDir/$_/bins.rast.json" } readdir $dh;
 closedir $dh; undef $dh;
-print STDERR scalar(@binJobs) . " bin jobs found.\n";
-# Print a header.
-print "Sample\tGood\tBad\n";
+print STDERR scalar(@binJobs) . " completed bin jobs found.\n";
+# This hash will store a list of binning-data tuples by site. Each tuple will be
+# [bpTotal, #good, #bad, bpBinned, bpUnbinned].
+my %report;
 # Loop through the bins.
 for my $binJob (@binJobs) {
     $stats->Add(binFound => 1);
     my $jobDir = "$binDir/$binJob";
+    # Initialize our stats for this bin.
+    my ($site, $good, $bad, $bpTotal, $bpBinned) = ('(Unspecified)', 0, 0, 0, 0);
     if (! -s "$jobDir/site.tbl") {
-        $stats->Add(binSiteless => 1);
+        $stats->Add(jobSiteless => 1);
     } else {
         # Check the site.
         open(my $sh, "<$jobDir/site.tbl") || die "Could not open site file: $!";
         my $line = <$sh>;
-        my ($proj, $site) = split /\t/, $line;
-        if ($site ne $siteName) {
-            # The bin is for the wrong site.
-            $stats->Add(jobWrongSite => 1);
-        } elsif (-z "$jobDir/bins.rast.json") {
-            # Here the bin job is for the site, but has no bins.
-            $stats->Add(jobNoGenomes => 1);
-        } else {
-            # Here we have a bin job in the correct site which produced genomes.
-            $stats->Add(jobWithGenomes => 1);
-            print STDERR "Processing $binJob.\n";
-            # This will be set to TRUE when we have processed all the bins.
-            my $done;
-            # This will be the current bin number.
-            my $idx = 1;
-            # This will count the good and bad bins.
-            my ($good, $bad) = (0, 0);
-            # Loop through the bins.
-            while (! $done) {
-                # Check for a bin GTO.
-                if (-s "$jobDir/bin$idx.gto") {
-                    # We have one. Extract the ID and check for quality.
-                    my $gto = GenomeTypeObject->create_from_file("$jobDir/bin$idx.gto");
-                    my $id = $gto->{id};
-                    if ($goods{$id}) {
-                        $good++;
-                        $stats->Add(binsGood => 1);
-                    } else {
-                        $bad++;
-                        $stats->Add(binsBad => 1);
-                    }
-                    # Get the next one.
-                    $idx++;
+        (undef, $site) = split /\t/, $line;
+    }
+    if (-z "$jobDir/bins.rast.json") {
+        # Here the bin job is for the site, but has no bins.
+        $stats->Add(jobNoGenomes => 1);
+    } else {
+        # Here we have a bin job in the correct site which produced genomes.
+        $stats->Add(jobWithGenomes => 1);
+        print STDERR "Processing $binJob.\n";
+        # This will be set to TRUE when we have processed all the bins.
+        my $done;
+        # This will be the current bin number.
+        my $idx = 1;
+        # Loop through the bins.
+        while (! $done) {
+            # Check for a bin GTO.
+            if (-s "$jobDir/bin$idx.gto") {
+                # We have one. Extract the ID and check for quality.
+                my $gto = GenomeTypeObject->create_from_file("$jobDir/bin$idx.gto");
+                my $id = $gto->{id};
+                if ($goods{$id}) {
+                    $good++;
+                    $stats->Add(binsGood => 1);
                 } else {
-                    # We don't have one, so we are done.
-                    $done = 1;
+                    $bad++;
+                    $stats->Add(binsBad => 1);
                 }
+                # Count the contigs.
+                for my $contig (@{$gto->{contigs}}) {
+                    $bpBinned += length($contig->{dna});
+                }
+                # Get the next one.
+                $idx++;
+            } else {
+                # We don't have one, so we are done.
+                $done = 1;
             }
-            print "$binJob\t$good\t$bad\n";
+        }
+        # Now count the total base pairs for the sample.
+        open(my $ih, "<$jobDir/contigs.fasta") || die "Could not open contigs.fasta for $jobDir: $!";
+        while (! eof $ih) {
+            my $line = <$ih>;
+            chomp $line;
+            if ($line =~ /^>/) {
+                $stats->Add(sampleContigsRead => 1);
+            } else {
+                $bpTotal += length($line);
+            }
+        }
+        # Save the results.
+        push @{$report{$site}}, [$bpTotal, $good, $bad, $bpBinned, $bpTotal - $bpBinned];
+    }
+}
+# Now we process the output, one site at a time.
+print STDERR "Producing reports.\n";
+for my $site (sort keys %report) {
+    # Sort the site data by total base pairs.
+    my @tuples = sort { $a->[1] <=> $b->[1] } @{$report{$site}};
+    # Accumulate totals for the summary.
+    my ($worked, $total) = (0, 0);
+    my @totals = (0, 0, 0, 0, 0);
+    print join(" ", map { uc $_ } split /_/, $site) . " Binning Results\n";
+    print join("\t", qw(size good bad binned unbinned) ) . "\n";
+    for my $tuple (@tuples) {
+        # Print this job.
+        print join("\t", @$tuple) . "\n";
+        for (my $i = 0; $i < 5; $i++) {
+            $totals[$i] += $tuple->[$i];
+        }
+        # Count this job (the job worked if the bpBinned > 0).
+        $total++;
+        if ($tuple->[3] > 0) {
+            $worked++;
         }
     }
+    # Finish the site report.
+    print join("\t", '----------', '---', '---', '----------', '----------') . "\n";
+    print join("\t", @totals) . "\n";
+    print "$total jobs, $worked found bins.\n\n";
 }
 print STDERR "All done.\n" . $stats->Show();
