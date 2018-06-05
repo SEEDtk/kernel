b49aa36a4bc9a10dc2ba7d27145e07852a877418
Bruce Parrello <bparrello@figresearch.com>
2018-06-05 14:48:22 -0500

    New eval scripts

---
+++ scripts/eval_gtos.py
@@ -0,0 +1,125 @@
+from __future__ import print_function  # <-- this lets python2 use python3's print function
+
+import sys, time, os, subprocess
+from distutils import dir_util  # handle dirs
+import argparse
+import shutil
+import numpy as np
+from sklearn.externals import joblib
+
+def run_predictor(n_col):
+	y = X_all[:, n_col]
+	X = np.delete(X_all, n_col, axis=1)
+	role_ID = col_names[n_col]
+	
+	clfDir = args.trainDir + "/Predictors/" + role_ID + "/Classifiers/" + args.clfType
+	clfFile = clfDir + "/"  + args.clfType
+	ldaFile = clfDir + "/LDA_vars"
+
+	if args.LDA: 
+		vars_to_use = np.genfromtxt(ldaFile, delimiter='\t', dtype=int)
+		X_feat = X[:,vars_to_use]
+		if vars_to_use.shape[0] == 1:
+			X_feat = X_feat.reshape(-1, 1)
+		
+	else:
+		X_feat = X
+
+	clf = joblib.load(clfFile)
+	results = clf.predict(X_feat)
+	results = results.tolist()
+	results.append(n_col)
+	print("Completed %d: %s predictor." % (n_col, role_ID))
+	return results
+
+stime = time.time()
+
+parser = argparse.ArgumentParser(description='Evaluate a list of gtos for consistency.')
+parser.add_argument("trainDir", help="Directory which contains built predictors",
+                    type=str)
+parser.add_argument("testDir", help="Directory for output files",
+                    type=str)
+parser.add_argument("gtoList", help="Plain text list of gto files to evaluate",
+                    type=str)
+parser.add_argument("roles_in_subsystems", help="List of roles in subsystems",
+                    type=str)
+parser.add_argument("roles_to_use", help="List of roles to use in evaluations",
+                    type=str)
+parser.add_argument("-c", "--classifier", dest="clfType", default="RandomForestClassifier", help="Type of sklearn classifier to use")
+parser.add_argument("--clear", action="store_true",
+                  help="Clear files from testDir so that new output can be stored")
+parser.add_argument("--LDA", action="store_true",
+                  help="Use Linear Discriminant Analysis")
+args = parser.parse_args()
+
+if not os.path.isdir(args.testDir):
+    sys.stderr.write("not a valid testDir: %s\n" % (args.testDir))
+    sys.exit(-1)
+
+if args.clear:
+	print("Clearing " + args.testDir + "/summaries...")
+	if os.path.isdir(args.testDir + "/summaries"):
+		shutil.rmtree(args.testDir + "/summaries")
+	if os.path.isdir(args.testDir + "/predictions"):
+		shutil.rmtree(args.testDir + "/predictions")
+	retcode = subprocess.call(["./gtos_to_matrix.pl", args.gtoList, args.testDir, args.roles_in_subsystems, args.roles_to_use, "--clear"])
+
+	if retcode:
+		sys.exit(-1)
+	print("Generation of matrix finished in %0.2f seconds." % (time.time() - stime))
+
+X_all = np.genfromtxt(args.testDir + '/X', delimiter='\t')
+col_names = np.genfromtxt(args.testDir + '/col.h', delimiter='\t', dtype=str)
+genomes = np.genfromtxt(args.testDir + '/row.h', delimiter='\t', dtype=str)
+
+
+#...genfromtxt reformats a 1-row dataset as a vector, not an array,
+#   so the number of dimensions must be reformatted if not 2D array.
+if len(X_all.shape) != 2:
+	X_all = X_all.reshape(1,-1)
+if len(genomes.shape) != 1:
+	genomes = genomes.reshape(1)
+
+#X_all[X_all > 5.] = 6.
+
+predictions = joblib.Parallel(n_jobs=32)(joblib.delayed(run_predictor)(n_col) for n_col in range(X_all.shape[1]))
+
+predictions = np.asarray(predictions)
+predictions = np.transpose(predictions)
+col_ind = np.asarray(predictions[-1], dtype=int)
+predictions = np.delete(predictions, -1, axis=0)
+predictions = predictions[:,col_ind]
+
+real_present = X_all > 0
+pred_present = predictions > 0
+
+coarse_const = 100.0*np.mean(real_present == pred_present, axis=1)
+fine_const = 100.0*np.mean(X_all == predictions, axis=1)
+score_table = genomes.reshape(-1, 1)
+score_table = np.hstack((score_table, coarse_const.round(2).reshape(-1,1)))
+score_table = np.hstack((score_table, fine_const.round(2).reshape(-1,1)))
+
+if not os.path.isdir(args.testDir + "/summaries"):
+	os.mkdir(args.testDir + "/summaries")
+#if not os.path.isdir(args.testDir + "/predictions"):
+#	os.mkdir(args.testDir + "/predictions")
+
+for n_row in range(X_all.shape[0]):
+	gtoID = genomes[n_row]
+	gto_sum_file = args.testDir + "/summaries/" + gtoID + ".out"
+	summary = ["Coarse Consistency: " + str(np.round(coarse_const[n_row], decimals = 1))]
+	summary.append("Fine Consistency: " + str(np.round(fine_const[n_row], decimals = 1)))
+	for n_col in range(X_all.shape[1]):
+		n_pred = predictions[n_row, n_col]
+		n_real = X_all[n_row, n_col]
+		
+		if n_pred != n_real:
+			summary.append(col_names[n_col] + "\t" + str(X_all[n_row, n_col]) + "\t" + str(predictions[n_row, n_col]))
+
+	np.savetxt(gto_sum_file, summary, fmt="%s", delimiter='\t')
+
+pred_file = args.testDir + "/predictions"
+score_file = args.testDir + "/scores"
+np.savetxt(score_file, score_table, fmt="%s", delimiter='\t')
+np.savetxt(pred_file, predictions, fmt="%d", delimiter='\t')
+print("Finished %d evaluations with %d roles in %0.2f seconds." % (predictions.shape[0], predictions.shape[1], time.time()-stime))
---
+++ scripts/gtos_to_matrix.pl
@@ -0,0 +1,161 @@
+#!/usr/bin/env perl
+=head1 GTO Files to Role Matrix
+
+    gtos_to_matrix.pl gtoList probDir roles.in.subsystems roles.to.use
+
+This program extracts the roles from a L<GenomeTypeObject> and computes the role IDs. It produces a file containing the unmapped roles on the standard error output. Three output files are produced: a matrix 'X' of role counts with genomes as rows and roles as columns, a list of GTO names 'row.h', and a list of role IDs used 'col.h'.
+
+=head2 Parameters
+
+The positional parameters are a file containing a list of GTO input files (in json format), the directory in which to place the output files and the name of a file containing the IDs of roles to keep (produced by L<build_role_tables.pl>).
+
+=cut
+
+use strict;
+use warnings;
+use Data::Dumper;
+use GenomeTypeObject;
+use SeedUtils;
+use ScriptUtils;
+use RoleParse;
+use Data::Dump qw(pp);
+use File::Copy::Recursive;
+
+my $opt = ScriptUtils::Opts('gto_file probDir roles.in.subsystems roles.to.use', 
+	['clear', 'overwrite previous results']);
+        
+my ($gto_list, $probDir, $roles_in_subsystems, $roles_to_use) = @ARGV;
+my (%roleIDs);
+
+if (!-d $probDir) 
+{
+    mkdir($probDir) or die "Could not create probDir '$probDir'";
+}
+
+elsif ($opt->clear) 
+{
+    print "Clearing $probDir.\n";
+    
+    if (-d "$probDir/Predictors") 
+    {
+        File::Copy::Recursive::pathrmdir("$probDir/Predictors") 
+        	|| die "Could not clear Predictors directory: $!\ ";
+    }
+    
+    my @files = qw(col.h roles.mapped roles.not_mapped row.h X);
+    
+    for my $file (@files) 
+    {
+        if (-f "$probDir/$file") 
+        {
+            unlink "$probDir/$file";
+        }
+    }
+}
+
+else 
+{
+    die "ERROR: probDir '$probDir' already exists";
+}
+
+die "Input file '$roles_to_use' does not exist" unless (-s $roles_to_use);
+
+open my $gto_fh, "<", "$gto_list"
+    or die "Could not read-open '$gto_list'";
+
+open my $X_fh,     '>', "$probDir/X"   
+	or die "Could not write-open '$probDir/X'";
+	
+open my $row_fh,   '>', "$probDir/row.h"
+	or die "Could not write-open '$probDir/row.h'";
+	
+open my $col_fh,   '>', "$probDir/col.h"
+	or die "Could not write-open '$probDir/col.h'";
+
+open my $unmapped_fh,   '>', "$probDir/roles.not_mapped"
+	or die "Could not write-open '$probDir/roles.not_mapped'";
+
+open my $roles_fh,   '<', "$roles_to_use"
+	or die "Could not read-open '$roles_to_use'";
+
+my %roleMap = map { my ($roleID, $checksum) = split /\t/;
+	($checksum => $roleID)} &SeedUtils::file_read($roles_in_subsystems);
+
+my %mapRoles = reverse %roleMap;
+
+my $role_count = 0;
+
+while(<$roles_fh>)
+{
+	chomp(my $ID = $_);
+	my $checksum = $mapRoles{$ID};
+	$roleIDs{$checksum} = $ID;
+	print $col_fh "$role_count\t$ID\n";
+	$role_count = $role_count + 1;
+}
+
+my $genome_count = 0;
+
+while(<$gto_fh>)
+{
+	chomp(my $gto = $_);
+	die "Input file '$gto' does not exist" unless (-s $gto);
+	my @dirs = split(/\//,$gto);
+	my $genome = $dirs[-1];
+	
+	foreach (@dirs)
+	{
+		if (m/([0-9]+\.[0-9]+)/)
+		{
+			$genome = $1;
+		}
+	}
+	
+	print $row_fh "$genome_count\t$genome\n";
+	$genome_count = $genome_count + 1;
+
+	my $proc = GenomeTypeObject->new({file => $gto});
+
+	my @CDSs = map { [ $_->{id}, $_->{function} ] } grep {
+		($_->{type} eq q(CDS)) || ($_->{id} =~ m{\.peg\.}) } $proc->features();
+	
+	my %counts;
+	
+	foreach my $checksum (keys %roleIDs)
+	{
+		$counts{$roleIDs{$checksum}} = 0;
+	}
+	
+	foreach my $cds (@CDSs) 
+	{
+	    my ($fid, $func) = @$cds;
+	    my @roles = &SeedUtils::roles_of_function($func);
+	    
+	    foreach my $role (@roles) 
+	    {
+	        # Compute the role's checksum.
+	        my $checksum = RoleParse::Checksum($role);
+	        # Compute the ID for this checksum.
+	        my $id = $roleIDs{$checksum};
+	        
+	        if ($id) 
+	        {
+	            $counts{$id}++;
+	        }
+	        
+	        else 
+	        {
+	            print $unmapped_fh "$gto role not mapped:\t$fid\t$role\n";
+	        }
+	    }
+	}
+	
+	my @role_counts;
+	
+	for my $ID (sort keys %counts) 
+	{
+	    push(@role_counts, $counts{$ID});
+	}
+	
+	print $X_fh join("\t", @role_counts) . "\n";
+}
