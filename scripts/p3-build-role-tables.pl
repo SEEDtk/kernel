f797c86bda31f5b1f517ecfbba4cbec1d43e2f0a
Bruce Parrello <bparrello@figresearch.com>
2018-07-09 13:38:19 -0500

    New version of build-role-tables for when subsystem directories go bad.

--- scripts/p3-build-role-tables.pl
+++ scripts/p3-build-role-tables.pl
@@ -29,7 +29,7 @@ use SeedUtils;
 
 =head1 Build Role Tables From the Patric Snapshot
 
-    p3-build-role-tables.pl [ options ] annotations subsystems outDir
+    p3-build-role-tables.pl [ options ] outDir annotations subsystems
 
 This script builds the same role tables output by L<build_role_tables.pl>, but builds them from the PATRIC
 kmer snapshot rather than the Shrub. The goal is to create something closer to the data used by RASTtk.
@@ -46,16 +46,15 @@ The Shrub database is used to convert checksums to role IDs.
 Note that the C<roles.to.use> file will be restricted to non-hypothetical roles that occur between 1 to 5 times in over 100
 genomes.
 
-
-
 =head2 Parameters
 
-The positional parameters are the name of the assignment directory, the name of the subsystem directory, and
-the name of the output directory.
+The positional parameters are the name of the output directory, the name of the assignment directory, and
+the name of the subsystem directory.
 
 Annotations are in C</vol/core-seed/kmers/core.>I<XXXX-XXXX>C</Annotations/0>. Subsystems are in
 C</vol/patric3/fams/>I<XXXX-XXXX>C</subsystem-import/subsystems.tgz>. In both cases I<XXXX-XXXX> is
-the release date. The subsystems need to be decompressed before processing.
+the release date. The subsystems need to be decompressed before processing. If the subsystem directory
+is omitted, then it is assumed C<roles.in.subsystems> already exists in the output directory.
 
 The command-line options are those found in L<Shrub/script_options>.
 
@@ -63,7 +62,7 @@ The command-line options are those found in L<Shrub/script_options>.
 
 $| = 1;
 # Get the command-line parameters.
-my $opt = ScriptUtils::Opts('annotations subsystems outDir',
+my $opt = ScriptUtils::Opts('outDir annotations subsystems',
         Shrub::script_options(),
         );
 my $stats = Stats->new();
@@ -77,91 +76,104 @@ if (! $annotations) {
     die "No annotation directory specified.";
 } elsif (! -d $annotations) {
     die "Annotation directory $annotations not found.";
-} elsif (! $subsystems) {
-    die "No subsystem directory specified.";
-} elsif (! -d $subsystems) {
-    die "Subsystem directory $subsystems not found.";
 } elsif (! $outDir) {
     die "No output directory specified.";
 } elsif (! -d $outDir) {
     die "Output directory $outDir not found.";
 }
-# Get the list of subsystem directories.
-print "Processing subsystems in $subsystems.\n";
-opendir(my $sdh, $subsystems) || die "Could not open $subsystems: $!";
-my @subsystems = grep { -s "$subsystems/$_/spreadsheet" } readdir $sdh;
-closedir $sdh;
-print scalar(@subsystems) . " subsystems found.\n";
+if (! $subsystems && ! -s "$outDir/roles.in.subsystems") {
+    die "No subsystem directory specified and no roles.in.subsystems present.";
+} elsif (! -d $subsystems) {
+    die "Subsystem directory $subsystems not found.";
+}
 # This hash will contain the roles found in subsystems. For each checksum, it will contain the role ID and name.
 my %roleHash;
-# Loop through the subsystems.
-for my $subsystem (@subsystems) {
-    my $dirName = "$subsystems/$subsystem";
-    if (! CopyFromSeed::ReadFlagFile("$dirName/EXCHANGABLE")) {
-        print "Subsystem $subsystem is private in SEED.\n";
-        $stats->Add(subsystemPrivate => 1);
-    } else {
-        # Check the classification for an experimental subsystem.
-        my $ok = 1;
-        if (open(my $ch, "$dirName/CLASSIFICATION")) {
-            my $line = <$ch>;
-            if ($line =~ /experimental/i) {
-                $ok = 0;
-                print "Subsystem $subsystem is experimental.\n";
-                $stats->Add(subsystemExperimental => 1);
+# Get the list of subsystem directories.
+if ($subsystems) {
+    print "Processing subsystems in $subsystems.\n";
+    opendir(my $sdh, $subsystems) || die "Could not open $subsystems: $!";
+    my @subsystems = grep { -s "$subsystems/$_/spreadsheet" } readdir $sdh;
+    closedir $sdh;
+    print scalar(@subsystems) . " subsystems found.\n";
+    # Loop through the subsystems.
+    for my $subsystem (@subsystems) {
+        my $dirName = "$subsystems/$subsystem";
+        if (! CopyFromSeed::ReadFlagFile("$dirName/EXCHANGABLE")) {
+            print "Subsystem $subsystem is private in SEED.\n";
+            $stats->Add(subsystemPrivate => 1);
+        } else {
+            # Check the classification for an experimental subsystem.
+            my $ok = 1;
+            if (open(my $ch, "$dirName/CLASSIFICATION")) {
+                my $line = <$ch>;
+                if ($line =~ /experimental/i) {
+                    $ok = 0;
+                    print "Subsystem $subsystem is experimental.\n";
+                    $stats->Add(subsystemExperimental => 1);
+                }
             }
-        }
-        if ($ok) {
-            # This is a real subsystem. Process it.
-            print "Reading roles from $subsystem.\n";
-            open(my $sh, "<$dirName/spreadsheet") || die "Could not open $subsystem spreadsheet: $!";
-            my $line = <$sh>;
-            while ($line && $line =~ /^\S+\t(.+)/) {
-                my $function = $1;
-                $stats->Add(roleLineIn => 1);
-                my @roleNames = SeedUtils::roles_of_function($function);
-                for my $roleName (@roleNames) {
-                    my $checksum = RoleParse::Checksum($roleName);
-                    if ($roleHash{$checksum}) {
-                        # Here we have already stored this role.
-                        $stats->Add(roleDuplicate => 1);
-                    } else {
-                        my ($roleID) = $shrub->GetFlat('Role', 'Role(checksum) = ? AND Role(hypo) = ?', [$checksum, 0], 'id');
-                        if(! $roleID) {
-                            # Here we have a role that is no longer supported, which is very bad.
-                            print "WARNING: No role ID found for $roleName.\n";
-                            $stats->Add(roleNotFound => 1);
+            if ($ok) {
+                # This is a real subsystem. Process it.
+                print "Reading roles from $subsystem.\n";
+                open(my $sh, "<$dirName/spreadsheet") || die "Could not open $subsystem spreadsheet: $!";
+                my $line = <$sh>;
+                while ($line && $line =~ /^\S+\t(.+)/) {
+                    my $function = $1;
+                    $stats->Add(roleLineIn => 1);
+                    my @roleNames = SeedUtils::roles_of_function($function);
+                    for my $roleName (@roleNames) {
+                        my $checksum = RoleParse::Checksum($roleName);
+                        if ($roleHash{$checksum}) {
+                            # Here we have already stored this role.
+                            $stats->Add(roleDuplicate => 1);
                         } else {
-                            $roleHash{$checksum} = [$roleID, $roleName];
-                            $stats->Add(roleStored => 1);
+                            my ($roleID) = $shrub->GetFlat('Role', 'Role(checksum) = ? AND Role(hypo) = ?', [$checksum, 0], 'id');
+                            if(! $roleID) {
+                                # Here we have a role that is no longer supported, which is very bad.
+                                print "WARNING: No role ID found for $roleName.\n";
+                                $stats->Add(roleNotFound => 1);
+                            } else {
+                                $roleHash{$checksum} = [$roleID, $roleName];
+                                $stats->Add(roleStored => 1);
+                            }
                         }
                     }
+                    $line = <$sh>;
                 }
-                $line = <$sh>;
+                $stats->Add(subsystemProcessed => 1);
             }
-            $stats->Add(subsystemProcessed => 1);
         }
     }
-}
-my $roleCount = scalar keys %roleHash;
-if ($roleCount == 0) {
-    die "No roles found.\n";
+    my $roleCount = scalar keys %roleHash;
+    if ($roleCount == 0) {
+        die "No roles found.\n";
+    } else {
+        print "$roleCount roles found.\n";
+    }
+    # All the subsystem roles are done. Build roles.in.subsystems.
+    open(my $oh, ">$outDir/roles.in.subsystems") || die "Could not open roles.in.subsystems: $!";
+    for my $checksum (sort keys %roleHash) {
+        my ($roleID, $roleName) = @{$roleHash{$checksum}};
+        print $oh join("\t", $roleID, $checksum, $roleName) . "\n";
+    }
+    close $oh; undef $oh;
 } else {
-    print "$roleCount roles found.\n";
+    # Here we read the roles from an existing roles.in.subsystems.
+    open(my $ih, "<$outDir/roles.in.subsystems") || die "Could not open roles.in.subsystems: $!";
+    while (! eof $ih) {
+        my $line = <$ih>;
+        if ($line =~ /^(\S+)\t(\S+)\t(.+)/) {
+            $roleHash{$2} = [$1, $3];
+        }
+    }
+    print scalar(keys %roleHash) . " roles read from file.\n";
 }
-# All the subsystem roles are done. Build roles.in.subsystems.
-open(my $oh, ">$outDir/roles.in.subsystems") || die "Could not open roles.in.subsystems: $!";
-for my $checksum (sort keys %roleHash) {
-    my ($roleID, $roleName) = @{$roleHash{$checksum}};
-    print $oh join("\t", $roleID, $checksum, $roleName) . "\n";
-}
-close $oh; undef $oh;
 # Now we build the raw table. Get the genomes.
 opendir(my $adh, $annotations) || die "Could not open annotations directory: $!";
 my @genomes = sort grep { $genomes{$_} && -s "$annotations/$_" } readdir $adh;
 closedir $adh;
 # Open the output file.
-open($oh, ">$outDir/raw.table") || die "Could not open raw.table: $!";
+open(my $oh, ">$outDir/raw.table") || die "Could not open raw.table: $!";
 # The role counts will be stored in here.
 my %roleCount;
 # Loop through the genomes.
