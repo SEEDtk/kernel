3c94d767f07a832f36f5492b067b4118ba32cb00
Bruce Parrello <bparrello@figresearch.com>
2019-02-17 17:22:32 -0600

    Add resume option.

--- scripts/p3-rep-role-distance.pl
+++ scripts/p3-rep-role-distance.pl
@@ -19,6 +19,10 @@ Additional command-line options are those in L<EvalCon/role_options> plus the fo
 
 Display progress messages on STDERR.
 
+=item resume
+
+Resume after a previous failed run.  The parameter should be the ID of the last genome successfully processed.
+
 =back
 
 =cut
@@ -37,10 +41,12 @@ $| = 1;
 # Get the command-line options.
 my $opt = P3Utils::script_opts('repDir', EvalCon::role_options(),
         ['verbose|debug|v', 'display progress messages on STDERR'],
+        ['resume=s', 'resume after specified genome']
         );
 # Get the options.
 my $debug = $opt->verbose;
 my $logH = ($debug ? \*STDERR : undef);
+my $resume = $opt->resume;
 # Get access to PATRIC.
 print STDERR "Connecting to PATRIC.\n" if $debug;
 my $p3 = P3DataAPI->new();
@@ -69,58 +75,68 @@ print STDERR "$totalGroups groups found in directory.\n" if $debug;
 P3Utils::print_cols([qw(group name size minDist meanDist maxDist sdvDist meanSim sdvSim corr)]);
 # Loop through them.
 for my $genome0 (@$genomes) {
-    # Get this genome's representative object and the genomes in its group.
-    my $repGenome = $repDB->rep_object($genome0);
-    $nGroups++;
-    $stats->Add(groupIn => 1);
-    my $repList = $repGenome->rep_list();
-    my $name = $repGenome->name();
-    my $listCount = scalar(@$repList) + 1;
-    if ($listCount == 1) {
-        print STDERR "Skipping singleton group $genome0: $name.\n" if $debug;
-        $stats->Add(singletons => 1);
+    # Are we resuming?
+    if ($resume) {
+        # Skip until we find the specified resume genome.
+        $stats->Add(resumeSkip => 1);
+        if ($genome0 eq $resume) {
+            $resume = '';
+            print STDERR "Resuming after $genome0.\n" if $debug;
+        }
     } else {
-        print STDERR "Processing $genome0 ($nGroups of $totalGroups) with $listCount members: $name.\n" if $debug;
-        my %sims = map { $_->[0] => $_->[1] } @$repList;
-        # Get the GEOs for the representatives.
-        my $gHash = GEO->CreateFromPatric([$genome0, keys %sims], %gOpts);
-        # Get the basic GEO.
-        my $geo0 = $gHash->{$genome0};
-        if (! $geo0) {
-            print STDERR "Genome error on $genome0.\n" if $debug;
-            $stats->Add(groupSkipped => 1);
+        # Get this genome's representative object and the genomes in its group.
+        my $repGenome = $repDB->rep_object($genome0);
+        $nGroups++;
+        $stats->Add(groupIn => 1);
+        my $repList = $repGenome->rep_list();
+        my $name = $repGenome->name();
+        my $listCount = scalar(@$repList) + 1;
+        if ($listCount == 1) {
+            print STDERR "Skipping singleton group $genome0: $name.\n" if $debug;
+            $stats->Add(singletons => 1);
         } else {
-            # Loop through the members.
-            my (@distances, @sims);
-            my ($minDist, $maxDist) = (100, 0);
-            for my $genome (sort keys %$gHash) {
-                if ($genome ne $genome0) {
-                    my $geo = $gHash->{$genome};
-                    my $distance = $geo0->uni_similarity($geo);
-                    my $sim = $sims{$genome};
-                    push @distances, $distance;
-                    print STDERR "$genome distance is $distance, similarity is $sim.\n" if $debug;
-                    push @sims, $sim;
-                    $stats->Add(distanceComputed => 1);
-                    $minDist = $distance if $distance < $minDist;
-                    $maxDist = $distance if $distance > $maxDist;
-                }
-            }
-            print STDERR "Calculating statistics for $genome0.\n" if $debug;
-            # Now compute the stats and write them.
-            my $dV = vector(@distances);
-            my ($mean, $std) = (mean($dV), stddev($dV));
-            my $sV = vector(@sims);
-            my ($meanSim, $stdSim) = (mean($sV), stddev($sV));
-            $minDist = Math::Round::nearest(0.01, $minDist);
-            $maxDist = Math::Round::nearest(0.01, $maxDist);
-            my $corr;
-            if ($stdSim < 1 && $std <= 1) {
-                $corr = 1;
+            print STDERR "Processing $genome0 ($nGroups of $totalGroups) with $listCount members: $name.\n" if $debug;
+            my %sims = map { $_->[0] => $_->[1] } @$repList;
+            # Get the GEOs for the representatives.
+            my $gHash = GEO->CreateFromPatric([$genome0, keys %sims], %gOpts);
+            # Get the basic GEO.
+            my $geo0 = $gHash->{$genome0};
+            if (! $geo0) {
+                print STDERR "Genome error on $genome0.\n" if $debug;
+                $stats->Add(groupSkipped => 1);
             } else {
-                $corr = correlation($dV, $sV);
+                # Loop through the members.
+                my (@distances, @sims);
+                my ($minDist, $maxDist) = (100, 0);
+                for my $genome (sort keys %$gHash) {
+                    if ($genome ne $genome0) {
+                        my $geo = $gHash->{$genome};
+                        my $distance = $geo0->uni_similarity($geo);
+                        my $sim = $sims{$genome};
+                        push @distances, $distance;
+                        print STDERR "$genome distance is $distance, similarity is $sim.\n" if $debug;
+                        push @sims, $sim;
+                        $stats->Add(distanceComputed => 1);
+                        $minDist = $distance if $distance < $minDist;
+                        $maxDist = $distance if $distance > $maxDist;
+                    }
+                }
+                print STDERR "Calculating statistics for $genome0.\n" if $debug;
+                # Now compute the stats and write them.
+                my $dV = vector(@distances);
+                my ($mean, $std) = (mean($dV), stddev($dV));
+                my $sV = vector(@sims);
+                my ($meanSim, $stdSim) = (mean($sV), stddev($sV));
+                $minDist = Math::Round::nearest(0.01, $minDist);
+                $maxDist = Math::Round::nearest(0.01, $maxDist);
+                my $corr;
+                if ($stdSim < 1 && $std <= 1) {
+                    $corr = 1;
+                } else {
+                    $corr = correlation($dV, $sV);
+                }
+                P3Utils::print_cols([$genome0, $name, $listCount, $minDist, $mean, $maxDist, $std, $meanSim, $stdSim, $corr]);
             }
-            P3Utils::print_cols([$genome0, $name, $listCount, $minDist, $mean, $maxDist, $std, $meanSim, $stdSim, $corr]);
         }
     }
 }
