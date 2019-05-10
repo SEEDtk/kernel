7931f18e9c1db333ca9b3526384f1bc0d4d51411
Bruce Parrello <bparrello@figresearch.com>
2019-05-10 18:15:11 -0500

    Analysis of bin failures.

--- scripts/TestMaple.pl
+++ scripts/TestMaple.pl
@@ -1,45 +1,32 @@
 use strict;
 use FIG_Config;
-use GeoGroup;
 use Stats;
+use Bin::Blast;
 
 $| = 1;
 my $stats = Stats->new();
-my %options = (stats => $stats, detail => 0, logH => \*STDERR);
-P3Utils::print_cols(['score', 'Italian', 'Italian_name', 'Bin', 'Bin_name', 'Group']);
-# Comparing Italian binning sets with ours.  Italians in IT_Bins; ours in Bins_IT.
-opendir(my $th, 'IT_Bins') || die "Could not open italian directory: $!";
-my @itDirs = grep { substr($_,0,1) ne '.' && -d "IT_Bins/$_" } readdir $th;
-closedir $th;
-for my $itDir (@itDirs) {
-    print STDERR "Processing $itDir.\n";
-    if (! -d "Bins_IT/$itDir") {
-        print STDERR "No corresponding bin directory to $itDir.\n";
-        $stats->Add(missingDir => 1);
-    } else {
-        $stats->Add(foundDir => 1);
-        my $itGroup = GeoGroup->new(\%options, "IT_Bins/$itDir");
-        my $binGroup = GeoGroup->new(\%options, "Bins_IT/$itDir");
-        my ($pairs, $orphans1, $orphans2) = $itGroup->MapGroups($binGroup, 300);
-        $stats->Add(itExtras => scalar @$orphans1);
-        $stats->Add(binExtras => scalar @$orphans2);
-        for my $pair (@$pairs) {
-            my $itGeo = $itGroup->geo($pair->[0]);
-            my $binGeo = $binGroup->geo($pair->[1]);
-            my $output = 1;
-            if (! $itGeo->is_good) {
-                $stats->Add(badItGenome => 1);
-                $output = 0;
-            }
-            if (! $binGeo->is_good) {
-                $stats->Add(badBinGenome => 1);
-                $output = 0;
-            }
-            if ($output) {
-                P3Utils::print_cols([$pair->[2], $itGeo->id, $itGeo->name, $binGeo->id, $binGeo->name, $itDir]);
-                $stats->Add(pairFound => 1);
-            }
-        }
+my $workDir = "$FIG_Config::temp";
+open(my $ih, '<', 'empties.tbl') || die "Could not open input: $!";
+while (! eof $ih) {
+    my $sample = <$ih>;
+    chomp $sample;
+    print STDERR "Processing $sample.\n";
+    $stats->Add(samplesIn => 1);
+    my $blaster = Bin::Blast->new(undef, $workDir, "Bins_HMP/$sample/contigs.fasta",
+            maxE => 1e-20, minlen => 0.5, gap => 600);
+    # First, we need the list of bins and the locations where they hit the seed protein.
+    my $matches = {};
+    # We must search for the specified universal protein to create the initial bins.
+    # Get the list of genomes.
+    my $protFile = "$FIG_Config::global/seedprot.fa";
+    $matches = $blaster->FindProtein($protFile);
+    my $count = 0;
+    # Save the matches to a work file.
+    for my $contig (sort keys %$matches) {
+        my $match = $matches->{$contig};
+        print join("\t", $sample, $match->Contig, $match->Begin, $match->Dir, $match->Length) . "\n";
+        $count++;
+        $stats->Add('binsFound-lineOut' => 1);
     }
+    print STDERR "$count seeds found for $sample.\n";
 }
-print STDERR "All done.\n" . $stats->Show();
