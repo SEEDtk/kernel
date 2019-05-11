2306371daf2a46dde7ae19c6f1e88f9f22df4ea6
Bruce Parrello <bparrello@figresearch.com>
2019-05-11 11:27:41 -0500

    New binning progress audit.

--- scripts/bins_audit.pl
+++ scripts/bins_audit.pl
@@ -21,101 +21,117 @@ use strict;
 use warnings;
 use FIG_Config;
 use ScriptUtils;
-use Bin;
 use Stats;
-use Bin::Score;
 
 =head1 Audit Community Bins
 
-    bins_audit.pl [ options ] contigs json
+    bins_audit.pl [ options ] binDir
 
-This program performs an audit of the major bins in a binning run, where a major bin is defined here as one with
-multiple contigs. For each such bin, it displays the mean and standard deviation of the coverage for the bin, the
-coverage of the bin's seed contig, and a list of the missing universal roles.
+This program performs an analysis of the progress of the current binning effort.  For each type of sample,
+we will accumulate the number of good bins, the number of bad bins, the number of bins in progress,  the number of
+failed binning attempts, the number of unprocessed samples, the number of completed samples, and the number of
+samples in progress.  The hope is that this information can be used to compute the total number of expected good
+bins.
 
 =head2 Parameters
 
-The two positional parameters are the name of a contigs file (C<contigs.ref.bins> or C<contigs.bins>) and the name of
-a JSON file containing the final bins (C<bins.json> or C<bins.new.json>).
+The sole positional parameter is the name of the binning directory.  This must contain samples as subdirectories.
 
 The command-line options are the following.
 
 =over 4
 
-=item unifile
+=item verbose
 
-The name of a file containing the IDs of the universal roles. If omitted, the file C<uni_roles.tbl> in the C<Global>
-subdirectory of the data directory will be used. The file is tab-delimited, with the role IDs in the first column,
-occurrence counts in the second, and role names in the third.
+Write progress messages to STDERR.
 
 =back
 
 =cut
 
 # Get the command-line parameters.
-my $opt = ScriptUtils::Opts('contigFile binFile',
-                ['unifile=s',      'universal role file', { default => "$FIG_Config::global/uni_roles.tbl" }],
+my $opt = ScriptUtils::Opts('binDir',
+        ['verbose|debug|v', 'write progress to STDERR']
         );
 # Create the statistics object.
 my $stats = Stats->new();
-# Get the file names.
-my ($contigFile, $binFile) = @ARGV;
-if (! $contigFile) {
-    die "A contigs file name is required.";
-} elsif (! $binFile) {
-    die "A bin file name is required.";
+# Get the binning directory.
+my ($binDir) = @ARGV;
+if (! $binDir) {
+    die "No input directory specified.";
+} elsif (! -d $binDir) {
+    die "Binning directory $binDir is invalid or missing.";
 }
-# Get the universal roles.
-my $uniHash = Bin::Score::ReadUniHash($opt->unifile);
-# Read the bins.
-my $binList = Bin::ReadBins($binFile);
-$stats->Add(binsRead => scalar @$binList);
-# Loop through the bins, producing the report.
-for my $bin (@$binList) {
-    my @contigList = $bin->contigs;
-    if (scalar @contigList <= 1) {
-        $stats->Add(singletonBin => 1);
-    } else {
-        $stats->Add(goodBin => 1);
-        my $contig1 = $bin->contig1;
-        my $coverage1 = 0;
-        # Get the seed contig coverage.
-        # Loop through the contigs in the bin, computing the mean and standard deviation of the coverage.
-        my ($total, $squareT, $n, $cmin, $cmax) = (0, 0, 0, undef, 0);
-        my $contigList = $bin->all_contigs;
-        for my $contig (@$contigList) {
-            $stats->Add(contigInBin => 1);
-            my $c = $contig->coverage;
-            if ($contig->id eq $contig1) {
-                $coverage1 = $contig->covg;
-            }
-            $cmax = $c if $c > $cmax;
-            $cmin = $c if ! defined $cmin || $c < $cmin;
-            $total += $c;
-            $squareT += $c*$c;
-            $n++;
+# Get the options.
+my $debug = $opt->verbose;
+# This hash will track the totals for each site type.  The columns are
+# [site name, good-bins, bad-bins, accum-bins, fails, done, in-progress, not-started].
+use constant { SITE_NAME => 0, GOOD_BINS => 1, BAD_BINS => 2, ACCUM_BINS => 3, FAILS => 4,
+        DONE => 5, IN_PROGRESS => 6, NOT_STARTED => 7 };
+my %cats;
+# Loop through the binning directories.
+print STDERR "Reading $binDir.\n" if $debug;
+opendir(my $dh, $binDir) || die "Could not open binning directory: $!";
+while (my $sample = readdir $dh) {
+    my $subDir = "$binDir/$sample";
+    my $siteFile = "$subDir/site.tbl";
+    if (substr($sample, 0, 1) ne '.' && -s $siteFile) {
+        $stats->Add(sampleFound => 1);
+        open(my $ih, '<', $siteFile) || die "Could not open $siteFile: $!";
+        my $line = <$ih>;
+        close $ih; undef $ih;
+        chomp $line;
+        my (undef, undef, $cat) = split /\t/, $line;
+        print STDERR "Processing $subDir: $cat\n" if $debug;
+        if (! $cats{$cat}) {
+            $stats->Add(catFound => 1);
+            $cats{$cat} = [$cat, 0, 0, 0, 0, 0, 0, 0];
         }
-        my $m = $total / $n;
-        my $sd = sqrt($squareT / $n - $m * $m);
-        # Now compute the missing universal roles.
-        my $uniBin = $bin->uniProts;
-        my @missing;
-        for my $role (keys %$uniHash) {
-            if (! $uniBin->{$role}) {
-                push @missing, $uniHash->{$role};
-                $stats->Add(uniRoleMissing => 1);
+        my $counters = $cats{$cat};
+        # Are we done, in-progress, or not-started?
+        if (-s "$subDir/Eval/index.tbl") {
+            # Here we are done.  Did we fail to find any bins?
+            if (! -s "$subDir/ref.genomes.scores.tbl") {
+                $stats->Add(sampleFail => 1);
+                $counters->[FAILS]++;
             } else {
-                $stats->Add(uniRoleFound => 1);
+                $stats->Add(sampleDone => 1);
+                $counters->[DONE]++;
+                # Bins were found.  We count them.
+                open($ih, '<', "$subDir/Eval/index.tbl") || die "Could not open evaluation file for $sample: $!";
+                my $line = <$ih>;
+                while (! eof $ih) {
+                    $line = <$ih>;
+                    if ($line =~ /\t1$/) {
+                        $counters->[GOOD_BINS]++;
+                        $stats->Add(binsGood => 1);
+                    } else {
+                        $counters->[BAD_BINS]++;
+                        $stats->Add(binsBad => 1);
+                    }
+                }
             }
+        } elsif (-f "$subDir/sample.fasta") {
+            # Here we are in-progress.  Count the bins accumulating.
+            $stats->Add(sampleInProgress => 1);
+            $counters->[IN_PROGRESS]++;
+            my $i = 1;
+            while (-s "$subDir/bin$i.gto") {
+                $i++;
+            }
+            $i--;
+            $stats->Add(binsInProgress => $i);
+            $counters->[ACCUM_BINS]++;
+        } else {
+            # Here we are not started.
+            $stats->Add(sampleNotStarted => 1);
+            $counters->[NOT_STARTED]++;
         }
-        # Print the report on this bin.
-        print "BIN $contig1. Coverage = $m (+/- $sd, $n contigs ranging from $cmin to $cmax) from $coverage1.\n";
-        print "    Missing Roles\n";
-        print "    -------------\n";
-        for my $missing (@missing) {
-            print "     $missing\n";
-        }
-        print "\n\n";
     }
 }
-print "All done.\n" . $stats->Show();
+print STDERR "Producing report.\n" if $debug;
+print join("\t", qw(site name good_bins bad_bins accum_bins fail_samples done_samples in_progress not_started)) . "\n";
+for my $cat (sort keys %cats) {
+    print join("\t", @{$cats{$cat}}) . "\n";
+}
+print STDERR "All done.\n" . $stats->Show() if $debug;
