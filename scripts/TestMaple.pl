a7c9f6757ebd6c81710d38318caf2d71ecce5292
Bruce Parrello <bparrello@figresearch.com>
2019-07-14 13:48:09 -0500

    Method to copy good bins.

--- scripts/TestMaple.pl
+++ scripts/TestMaple.pl
@@ -5,14 +5,27 @@ use Bin;
 use SeedUtils;
 
 opendir(my $dh, 'Bins_HMP') || die "Could not open binning directory: $!";
-my @samples = grep { -s "Bins_HMP/$_/contigs.fasta" } readdir $dh;
+my @samples = grep { -s "Bins_HMP/$_/Eval/index.tbl" } readdir $dh;
 closedir $dh;
 for my $sample (@samples) {
     print STDERR "Processing $sample.\n";
     my $subDir = "Bins_HMP/$sample";
-    my $target = "Bins_Test/$sample";
-    my @files = qw(contigs.fasta site.tbl output.contigs2read.txt);
-    for my $file (@files) {
-        File::Copy::Recursive::fcopy("$subDir/$file", "$target/$file");
+    open(my $ih, '<', "$subDir/Eval/index.tbl") || die "Could not open index.tbl: $!";
+    my $line = <$ih>;
+    my %good;
+    while (! eof $ih) {
+        $line = <$ih>;
+        if ($line =~ /(\d+\.\d+).+1$/) {
+            $good{$1} = 1;
+        }
+    }
+    my $idx = 1;
+    while (-s "$subDir/bin$idx.gto") {
+        my $gto = SeedUtils::read_encoded_object("$subDir/bin$idx.gto");
+        my $id = $gto->{id};
+        if ($good{$id}) {
+            print "Copying bin $idx: $id\n";
+            File::Copy::Recursive("$subDir/bin$idx.gto", "GoodBins/$id.gto");
+        }
     }
 }
