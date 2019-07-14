218b059764fbd9ad6bd523c9b71b89e2bdc6e2f3
Bruce Parrello <bparrello@figresearch.com>
2019-07-14 01:26:08 -0500

    Test script to find assembled bins

--- scripts/TestMaple.pl
+++ scripts/TestMaple.pl
@@ -5,31 +5,14 @@ use Bin;
 use SeedUtils;
 
 opendir(my $dh, 'Bins_HMP') || die "Could not open binning directory: $!";
-my @samples = grep { -s "Bins_HMP/$_/bins.rast.json" } readdir $dh;
+my @samples = grep { -s "Bins_HMP/$_/contigs.fasta" } readdir $dh;
 closedir $dh;
 for my $sample (@samples) {
     print STDERR "Processing $sample.\n";
-    my $error = 0;
     my $subDir = "Bins_HMP/$sample";
-    my $binList = Bin::ReadBins("$subDir/bins.rast.json");
-    for (my $idx = 1; $idx <= scalar @$binList && ! $error; $idx++) {
-        my $bin = $binList->[$idx - 1];
-        my $gtoFile = "$subDir/bin$idx.gto";
-        if (! -s $gtoFile) {
-            $error++;
-            print STDERR "$gtoFile is missing for bin $idx.\n";
-        } else {
-            my $gto = SeedUtils::read_encoded_object($gtoFile);
-            my $gtoName = $gto->{scientific_name};
-            $gtoName =~ s/\s+cleaned//;
-            my $binName = $bin->name || "(unknown)";
-            if ($gtoName ne $binName) {
-                print STDERR "Bin $idx should be $binName but is $gtoName.\n";
-                $error++;
-            }
-        }
-    }
-    if ($error) {
-        print "$sample\n";
+    my $target = "Bins_Test/$sample";
+    my @files = qw(contigs.fasta site.tbl output.contigs2read.txt);
+    for my $file (@files) {
+        File::Copy::Recursive::fcopy("$subDir/$file", "$target/$file");
     }
 }
