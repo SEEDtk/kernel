103e39319a000b11112a03de9a2a4f2cf8fc4236
Bruce Parrello <bparrello@figresearch.com>
2019-12-06 09:46:57 -0500

    Search for bad FASTQ data.

--- scripts/TestMaple.pl
+++ scripts/TestMaple.pl
@@ -2,18 +2,11 @@ use strict;
 use FIG_Config;
 use SeedUtils;
 use File::Copy::Recursive;
+use FastQ;
 
-my @files = qw(GENOME TAXONOMY GENETIC_CODE TAXONOMY_ID);
-my $orgDir = '/vol/core-seed/FIGdisk/FIG/Data/Organisms';
-opendir(my $dh, $orgDir) || die "Could not open coreSEED directory: $!";
-my @genomes = grep { -d "$orgDir/$_" && $_ =~ /^(\d+\.\d+)$/ } readdir $dh;
-closedir $dh;
-for my $genome (@genomes) {
-    print "Processing $genome.\n";
-    File::Copy::Recursive::pathmk("FakeCoreOrgs/$genome") || die "Could not create $genome directory: $!";
-    for my $file (@files) {
-        if (-f "$orgDir/$genome/$file") {
-            File::Copy::Recursive::fcopy("$orgDir/$genome/$file", "FakeCoreOrgs/$genome") || die "Could not copy $genome $file: $!";
-        }
-    }
+my $fq = FastQ->new($ARGV[0]);
+my $count = 0;
+while ($fq->next()) {
+    $count++;
+    print "$count processed.\n" if $count % 4000 == 0;
 }
