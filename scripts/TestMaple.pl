06fbb99b910b056b9cdd7d413054e1f2de4e9462
Bruce Parrello <bparrello@figresearch.com>
2019-12-17 17:21:32 -0600

    First test script for assembly.

--- scripts/TestMaple.pl
+++ scripts/TestMaple.pl
@@ -1,12 +1,31 @@
 use strict;
 use FIG_Config;
-use SeedUtils;
-use File::Copy::Recursive;
-use FastQ;
+use SeedTkRun;
 
-my $fq = FastQ->new($ARGV[0]);
-my $count = 0;
-while ($fq->next()) {
-    $count++;
-    print "$count processed.\n" if $count % 40000 == 0;
+my ($dir) = @ARGV;
+open (my $dh, $dir) || die "Could not open directory $dir: $!";
+my @files = grep { $_ =~ /^bin\d+\.\d+_[12s]\.fastq/ } readdir $dh;
+closedir $dh;
+print scalar(@files) . " read files found.\n";
+my %groups;
+for my $file (@files) {
+    my ($prefix, $type) = $file =~ /(bin\d+\.\d+)_(.)/;
+    $groups{$prefix}{"-$type"} = $file;
+}
+chdir $dir;
+# Find the command.
+my $cmd = "spades.py";
+my $cmdPath = SeedTkRun::executable_for($cmd);
+die "Could not find $cmd." if ! $cmdPath;
+# Windows hack for paths with spaces.
+if ($cmdPath =~ /\s/) {
+    $cmdPath = $cmd;
+}
+my @groups = sort keys %groups;
+print scalar(@groups) . " bin groups found.\n";
+for my $group (sort keys %groups) {
+    print "Processing $group.\n";
+    my $parmH = $groups{$group};
+    my @parms = map { $_, $parmH->{$_} } keys %$parmH;
+    system($cmdPath, @parms);
 }
