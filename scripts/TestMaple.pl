111e4098fdc74c93cba25cd046270a877f2286f9
Bruce Parrello <bparrello@figresearch.com>
2020-02-27 08:57:13 -0600

    Script to count subsystem pattyfams.

--- scripts/TestMaple.pl
+++ scripts/TestMaple.pl
@@ -1,30 +1,28 @@
 use strict;
 use FIG_Config;
 
-$| = 1;
-my ($dir) = @ARGV;
-opendir(my $dh, $dir) || die "Could not open $dir: $!";
-my @bins = grep { -s "$dir/$_/run.log" } readdir $dh;
-closedir $dh;
-my ($count, $tot) = (0, scalar @bins);
-print STDERR "$tot bins found.\n";
-print "Bin\tsample\tfootprint\n";
-for my $bin (@bins) {
-    $count++;
-    print STDERR "Processing $bin ($count of $tot).\n";
-    open(my $ih, '<', "$dir/$bin/run.log") || die "Could not open log for $bin: $!";
-    my ($size, $done, $footprint) = ('', 0, 0);
-    while (! $done) {
-        my $line = <$ih>;
-        if (! defined $line || $line =~ /^SPAdes log can be found here/) {
-            $done = 1;
-        } elsif ($line =~ /\s+\d+G \/ (\d+)G\s+INFO/) {
-            $footprint = $1 if $footprint < $1;
-        } elsif ($line =~ /^Sample size is (\d+) gigabytes/) {
-            $size = $1;
-        }
-    }
-    if ($size) {
-        print "$bin\t$size\t$footprint\n";
+open(my $ih, '<', "subRoles.tbl") || die "Could not open subRoles.tbl: $!";
+my %roles;
+my $line = <$ih>;
+while (! eof $ih) {
+    if (<$ih> =~ /\t(.+)$/) {
+        $roles{$1} = 1;
     }
 }
+close $ih; undef $ih;
+my ($count, $kept) = 0;
+my %fams;
+open($ih, '<', '/vol/patric3/fams/2018-0531/merge1/prop.out/merged.families.1.1.nr-only') || die "Could not open families file: $!";
+while (! eof $ih) {
+    my $line = <$ih>;
+    $count++;
+    my ($fam, undef, undef, undef, undef, $role) = split /\t/, $line;
+    if ($roles{$role}) {
+        $fams{$fam} = 1;
+        $kept++;
+    }
+    if ($count % 5000 == 0) {
+        print STDERR "$count records process, $kept counted.\n";
+    }
+}
+print scalar(keys %fams) . " families found for " . scalar(keys %roles) . " subsystem roles in $kept records.\n";
