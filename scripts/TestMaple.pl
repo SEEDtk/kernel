f178586f8195c8d5894f9340bcd9006fc62d03e9
Bruce Parrello <bparrello@figresearch.com>
2020-08-14 20:53:19 -0500

    Core copy script.

--- scripts/TestMaple.pl
+++ scripts/TestMaple.pl
@@ -2,8 +2,14 @@ use strict;
 use FIG_Config;
 use File::Copy::Recursive;
 
-opendir(my $dh, "FakeCore") || die "Could not open FakeCore: $!";
-my @orgs = grep { $_ =~ /^\d+\.\d+/ } readdir $dh;
+if (-d "FakeCore") {
+    File::Copy::Recursive::pathempty("FakeCore");
+} else {
+    File::Copy::Recursive::pathmk("FakeCore");
+}
+opendir(my $dh, "/vol/core-seed/FIGdisk/FIG/Data/Organisms") || die "Could not open FakeCore: $!";
+my @orgs = grep { $_ =~ /^\d+\.\d+/ && -s "/vol/core-seed/FIGdisk/FIG/Data/Organisms/$_/assigned_functions" } readdir $dh;
+print scalar(@orgs) . " genomes found.\n";
 closedir $dh;
 for my $org (@orgs) {
     my $orgDir = "/vol/core-seed/FIGdisk/FIG/Data/Organisms/$org";
@@ -12,6 +18,9 @@ for my $org (@orgs) {
             print "Copying $file.\n";
             File::Copy::Recursive::fcopy("$orgDir/$file", "FakeCore/$org/$file") || die "Could not copy $org $file: $!";
         }
+        print "Copying $org.\n";
+        File::Copy::Recursive::fcopy("$orgDir/assigned_functions", "FakeCore/$org/assigned_functions") || die "Could not copy $org functions: $!";
+        File::Copy::Recursive::fcopy("$orgDir/GENOME", "FakeCore/$org/GENOME") || die "Could not copy $org genome name: $!";
     }
 }
 print "All done.\n";
