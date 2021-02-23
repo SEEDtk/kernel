68542cb4ba688624fdc6fd87978d89c56f9d8986
Bruce Parrello <bparrello@figresearch.com>
2021-02-22 20:47:57 -0600

    Analysis tool for sample composition.

--- scripts/TestBins.pl
+++ scripts/TestBins.pl
@@ -1,39 +1,63 @@
 use strict;
 use FIG_Config;
-use SeedTkRun;
-use File::Copy::Recursive;
+use GenomeTypeObject;
 
-my ($dir) = @ARGV;
-opendir(my $dh, $dir) || die "Could not open directory $dir: $!";
-my @files1 = readdir $dh;
+opendir(my $dh, "Bins_HMP") || die "Could not open Bins_HMP: $!";
+my @samples = grep { -s "Bins_HMP/$_/bin1.gto" } readdir $dh;
+print STDERR scalar(@samples) . " samples found.\n";
 closedir $dh;
-my %groups;
-for my $file (@files1) {
-    print "$file ";
-    if ($file =~ /^(bin\.\d+\.\d+)_([12s])\.fastq/) {
-        print "kept.\n";
-        $groups{$1}{"-$2"} = $file;
-    } else {
-        print "rejected.\n";
+print "sample\tsource\tgenome_id\tgenus\tinconsistent\tincomplete\tcontaminated\tother\tgood\timproved\n";
+for my $sample (sort @samples) {
+    my $source = getSource($sample);
+    my $qual = getQual($sample);
+    print STDERR "Processing $source sample $sample.\n";
+    opendir(my $sh, "Bins_HMP/$sample") || die "Could not open $sample directory: $!";
+    my @bins = sort map { "Bins_HMP/$sample/$_" } grep { $_ =~ /^bin\d+\.gto$/ } readdir $sh;
+    closedir $sh;
+    print STDERR scalar(@bins) . " bins found.\n";
+    for my $bin (@bins) {
+        print STDERR "Analyzing $bin.\n";
+        printBin($sample, $source, $qual, $bin);
     }
 }
-chdir $dir;
-# Find the command.
-my $cmd = "spades.py";
-my $cmdPath = SeedTkRun::executable_for($cmd);
-die "Could not find $cmd." if ! $cmdPath;
-# Windows hack for paths with spaces.
-if ($cmdPath =~ /\s/) {
-    $cmdPath = $cmd;
+
+sub getQual {
+    my ($sample) = @_;
+    my %retVal;
+    open(my $ih, '<', "Bins_HMP/$sample/Eval/index.tbl") || die "Could not open quality file for $sample: $!";
+    my $line = <$ih>;
+    while (! eof $ih) {
+        $line = <$ih>;
+        chomp $line;
+        my @fields = split /\t/, $line;
+        my $inconsistent = ($fields[9] < 87 ? "1" : "");
+        my $incomplete = ($fields[10] < 80 ? "1" : "");
+        my $contaminated = ($fields[11] > 10 ? "1" : "");
+        my $good = ($fields[14] ? "1" : "");
+        my $other = ((! $good && ! ($inconsistent || $incomplete || $contaminated)) ? "1" : "");
+        $retVal{$fields[1]} = [$inconsistent, $incomplete, $contaminated, $other, $good];
+    }
+    return \%retVal;
 }
-my @groups = sort keys %groups;
-print scalar(@groups) . " bin groups found.\n";
-for my $group (sort keys %groups) {
-    print "Processing $group.\n";
-    my $parmH = $groups{$group};
-    my @parms = map { $_, $parmH->{$_} } keys %$parmH;
-    File::Copy::Recursive::pathmk("asm.$group") || die "Could not make directory asm.$group: $!";
-    push @parms, '-o', "asm.$group";
-    print "Starting assembly for $group.\n";
-    system($cmdPath, @parms);
+
+sub getSource {
+    my ($sample) = @_;
+    my $retVal = "unknown";
+    if (open(my $ih, '<', "Bins_HMP/$sample/site.tbl")) {
+        my $line = <$ih>;
+        ($retVal) = ($line =~ /\t(\S+)/);
+    }
+    return $retVal;
+}
+
+sub printBin {
+    my ($sample, $source, $qual, $bin) = @_;
+    my $gto = GenomeTypeObject->create_from_file($bin);
+    my $genus = $gto->{ncbi_genus};
+    my $id = $gto->{id};
+    my $a = $gto->{analysis_events};
+    my @tools = grep { $_->{tool_name} eq "p3x-improve-gto" } @$a;
+    my $improved = (scalar(@tools) ? "1" : "");
+    my $q = $qual->{$id};
+    print join("\t", $sample, $source, $id, $genus, @$q, $improved) . "\n";
 }
