b36f32bf6894a053da462b694653c59370de10c2
Bruce Parrello <bparrello@figresearch.com>
2021-03-30 06:40:51 -0500

    New jar files.

--- scripts/TestMaple.pl
+++ scripts/TestMaple.pl
@@ -2,55 +2,28 @@ use strict;
 use FIG_Config;
 use File::Copy::Recursive;
 
-# parms are dir1 dir2 dirO
-
-# Tracks files already in the output
-my $outH;
-my ($dir1, $dir2, $dirO) = @ARGV;
-# Verify the output directory.
-if (! -d $dirO) {
-    File::Copy::Recursive::pathmk($dirO) || die "Could not create $dirO: $!";
-} else {
-    $outH = getFastQ($dirO);
-}
-# Get the two input directories.
-my $dir1H = getFastQ($dir1);
-my $dir2H = getFastQ($dir2);
-for my $file1 (sort keys %$dir1H) {
-    my $file2 = $file1;
-    $file2 =~ s/_L001/_L002/;
-    if ($dir2H->{$file2}) {
-        my $file3 = $file1;
-        $file3 =~ s/_L001/_L003/;
-        if (-s "$dirO/$file3") {
-            print "Skipping $file3-- already done.\n";
-        } else {
-            print "Copying $file1 and $file2 to $file3.\n";
-            open(my $oh, '>', "$dirO/$file3") || die "Could not open output file $file3: $!";
-            copyFile("$dir1/$file1", $oh);
-            copyFile("$dir2/$file2", $oh);
-            close $oh;
+opendir(my $dh, "Bins_HMP") || die "Could not open Bins_HMP: $!";
+my @dirs = grep { -s "Bins_HMP/$_/Eval/index.tbl" } readdir $dh;
+print "sample_id\tgood\tbad\n";
+for my $dir (@dirs) {
+    print STDERR "Processing $dir.\n";
+    my $subDir = "Bins_HMP/$dir";
+    if (open(my $ih, '<', "$subDir/site.tbl")) {
+        my $line = <$ih>;
+        close $ih; undef $ih;
+        if ($line =~ /HMP\s+stool/i) {
+            open($ih, '<', "$subDir/Eval/index.tbl") || die "Could not open index.tbl for $dir: $!";
+            $line = <$ih>;
+            my ($good, $bad) = (0, 0);
+            while (! eof $ih) {
+                $line = <$ih>;
+                if ($line =~ /1$/) {
+                    $good++;
+                } else {
+                    $bad++;
+                }
+            }
+            print "$dir\t$good\t$bad\n";
         }
     }
-}
-print "All done.\n";
-
-# Copy a file to the output.
-sub copyFile {
-    my ($file, $oh) = @_;
-    open(my $ih, '<', $file) || die "Could not open input file $file: $!";
-    while (! eof $ih) {
-        my $line = <$ih>;
-        print $oh $line;
-    }
-}
-
-
-# Return all the FASTQ files in the directory as a hash.
-sub getFastQ {
-    my ($dir) = @_;
-    opendir(my $dh, $dir) || die "Could not open directory $dir: $!";
-    my @files = grep { $_ =~ /\.fastq$/ } readdir $dh;
-    my %retVal = map { $_ => 1 } @files;
-    return \%retVal;
 }
