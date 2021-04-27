426d8adc05897d04bf45c4cf9a20d58a48a713a5
Bruce Parrello <bparrello@figresearch.com>
2021-04-26 20:13:27 -0500

    Script to copy sample contig files.

--- scripts/TestMaple.pl
+++ scripts/TestMaple.pl
@@ -2,28 +2,21 @@ use strict;
 use FIG_Config;
 use File::Copy::Recursive;
 
+my ($dest) = @ARGV;
 opendir(my $dh, "Bins_HMP") || die "Could not open Bins_HMP: $!";
-my @dirs = grep { -s "Bins_HMP/$_/Eval/index.tbl" } readdir $dh;
-print "sample_id\tgood\tbad\n";
+my @dirs = grep { -s "Bins_HMP/$_/contigs.fasta" } readdir $dh;
 for my $dir (@dirs) {
-    print STDERR "Processing $dir.\n";
-    my $subDir = "Bins_HMP/$dir";
-    if (open(my $ih, '<', "$subDir/site.tbl")) {
+    if (! open(my $ih, '<', "Bins_HMP/$dir/site.tbl")) {
+        print "$dir has no site file.\n";
+    } else {
         my $line = <$ih>;
-        close $ih; undef $ih;
-        if ($line =~ /HMP\s+stool/i) {
-            open($ih, '<', "$subDir/Eval/index.tbl") || die "Could not open index.tbl for $dir: $!";
-            $line = <$ih>;
-            my ($good, $bad) = (0, 0);
-            while (! eof $ih) {
-                $line = <$ih>;
-                if ($line =~ /1$/) {
-                    $good++;
-                } else {
-                    $bad++;
-                }
-            }
-            print "$dir\t$good\t$bad\n";
+        close $ih;
+        if ($line =~ /HMP\s+stool/) {
+            File::Copy::Recursive::fcopy("Bins_HMP/$dir/contigs.fasta", "$dest/$dir.fasta")
+                || die "Error copying $dir: $!";
+            print "$dir copied.\n";
+        } else {
+            print "$dir is of type $line";
         }
     }
 }
