0eaf7497ee05e17f1858ad68e56d22f3e9353953
Bruce Parrello <bparrello@figresearch.com>
2022-04-05 13:04:48 -0500

    Added quick repair script for fake fastas.

--- scripts/TestMaple.pl
+++ scripts/TestMaple.pl
@@ -2,21 +2,31 @@ use strict;
 use FIG_Config;
 use File::Copy::Recursive;
 
-my $inDir = '/vol/patric3/QA/applications';
-my $outDir = "$FIG_Config::data/p3Tests";
-File::Copy::Recursive::pathempty($outDir) || die "Could not empty $outDir: $!";
+my ($inDir) = @ARGV;
 opendir(my $dh, $inDir) || die "Could not open input directory: $!";
-my @appdirs = grep { $_ =~ /^App-/ && -d "$inDir/$_/tests" } readdir $dh;
-closedir $dh; undef $dh;
-for my $appdir (@appdirs) {
-    my $source = "$inDir/$appdir/tests";
-    my $appName = substr $appdir, 4;
-    print "Processing application $appName.\n";
-    opendir(my $dh, $source) || die "Could not open $source directory: $!";
-    my @jsons = grep { $_ =~ /\.json$/ } readdir $dh;
-    close $dh; undef $dh;
-    for my $json (@jsons) {
-        File::Copy::Recursive::fcopy("$source/$json", "$outDir/$appName.$json") || die "Copy failed for $json of $appName: $!";
+my @files = grep { $_ =~ /\.fna$/ } readdir $dh;
+closedir $dh;
+my $num = 1;
+my $fileCount = 0;
+my $lineCount = 0;
+my $changeCount = 0;
+for my $file (@files) {
+    $fileCount++;
+    open(my $ih, '<', "$inDir/$file") || die "Could not open $file: $!";
+    open(my $oh, '>', "$inDir/temp.fna") || die "Could not open output file: $!";
+    while (! eof $ih) {
+        my $line = <$ih>;
+        $lineCount++;
+        if ($line =~ /^>REP_(\d+\.\d+_)\d+(_.+)/) {
+            my $label = ">REP_$1$num$2";
+            print $oh "$label\n";
+            $changeCount++;
+            $num++;
+        } else {
+            print $oh $line;
+        }
     }
+    close $oh; close $ih;
+    File::Copy::Recursive::fmove("$inDir/temp.fna", "$inDir/$file") || die "Could not rename temp file: $!";
 }
-print "All done.";
+print "$fileCount files, $lineCount lines, $changeCount changed.\n";
