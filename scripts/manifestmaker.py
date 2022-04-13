686615805fcbc1cb33974d571bf009323eddd5ef
Tory <Tory@BLUEDEV>
2022-04-13 15:23:37 -0500

    format out the f strings

--- scripts/manifestmaker.py
+++ scripts/manifestmaker.py
@@ -19,12 +19,10 @@ with open(sample_list) as samples:
 manifest_path_on_maple = "/vol/mg-binning/Parkinsons/ManifestFiles/"
 
 #Creates a manifest file with the paths on maple
-with open(f"{manifest_path_on_maple}{author}_{sample_type}_manifest.txt", "w+") as manifest_file:
+with open(manifest_path_on_maple + author + "_" + sample_type + "_manifest.txt", "w+") as manifest_file:
     manifest_file.write("sample-id\tforward-absolute-filepath\treverse-absolute-filepath\n")
     for sample in sample_names:
-        manifest_file.write(f"{sample}\t"
-                            f"/vol/mg-binning/Parkinsons/{sample_type}/{author}/{sample}/{sample}_1.fastq\t"
-                            f"/vol/mg-binning/Parkinsons/{sample_type}/{author}/{sample}/{sample}_2.fastq")
+        manifest_file.write(sample + "\t" + "/vol/mg-binning/Parkinsons/" + sample_type + "/" + author + "/" + sample + "/" + sample + "_1.fastq\t/vol/mg-binning/Parkinsons/" + sample_type + "/" + author + "/" + sample + "/" + sample + "_2.fastq")
         #If the sample is NOT the last one on the list, put a new line at the end.
         if sample != sample_names[:-1]:
             manifest_file.write("\n")
