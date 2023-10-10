938ee441cc733643f242be6160e833a8e9111841
Bruce Parrello <bparrello@figresearch.com>
2023-10-10 09:34:49 -0500

    Removed invalid DLITs from output.

--- scripts/list_dlits.pl
+++ scripts/list_dlits.pl
@@ -7,13 +7,18 @@ eval {
 
 };
 if ($wrong_env) {
-    die "Invalid environment.  THis must be run in CoreSEED mode.";
+    die "Invalid environment.  This must be run in CoreSEED mode.";
 }
 my $fig = FIG->new();
 print STDERR "Requesting DLITs.\n";
 my $dlits = $fig->all_dlits();
 print STDERR scalar(@$dlits) . " DLITs found.\n";
 print "status\taa_sequence_md5\tpubmed\tcurator\tGO_code\n";
+my $out = 0;
 for my $dlit (@$dlits) {
-    print join("\t", @$dlit) . "\n";
+    if ($dlit->[1] =~ /^[a-f0-9]+$/) {
+        print join("\t", @$dlit) . "\n";
+        $out++;
+    }
 }
+print STDERR "$out DLITs printed.\n";
