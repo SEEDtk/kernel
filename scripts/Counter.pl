75fafc85956766d7b54386f5575b5592bf4fd09b
drake <drake@BLACK-DRAGON.mynetworksettings.com>
2024-03-20 23:46:51 -0500

    Removed Shrub dependencies.

--- scripts/Counter.pl
+++
@@ -1,9 +0,0 @@
-use Shrub;
-
-my $shrub = Shrub->new();
-my $count = $shrub->GetCount('Protein', '', []);
-print "$count proteins.\n";
-$count = $shrub->GetCount('Genome', '', []);
-print "$count genomes.\n";
-$count = $shrub->GetCount('Subsystem', '', []);
-print "$count subsystems.\n";
